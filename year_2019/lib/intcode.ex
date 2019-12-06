97b6a0ec48b98a69fa831c987b7a974d6fd408d7
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-12-06 12:47:17 -0600

    Refactor to use better names for things

--- year_2019/lib/intcode.ex
+++ year_2019/lib/intcode.ex
@@ -1,57 +1,57 @@
 defmodule Intcode do
   def build(s) do
     s
-    |> parse_opcodes
+    |> parse_machine
     |> assign_offsets(0, %{})
   end
 
-  def parse_opcodes(s) do
+  def parse_machine(s) do
     s
     |> String.split(",")
     |> Enum.map(&(Integer.parse(&1) |> elem(0)))
   end
 
-  def execute(opcodes), do: execute(opcodes, 0)
-  defp execute(opcodes, i) do
-    {_modes, opcode} = opcodes |> read(i) |> divrem(100)
+  def execute(machine), do: execute(machine, 0)
+  defp execute(machine, sp) do
+    {_modes, opcode} = machine |> read(sp) |> divrem(100)
 
     case opcode do
-      1 -> opcodes |> op(i, &Kernel.+/2) |> execute(i + 4)
-      2 -> opcodes |> op(i, &Kernel.*/2) |> execute(i + 4)
-      3 -> opcodes |> write_input(i) |> execute(i + 2)
-      4 -> opcodes |> output(i) |> execute(i + 2)
-      99 -> opcodes
+      1 -> machine |> op(sp, &Kernel.+/2) |> execute(sp + 4)
+      2 -> machine |> op(sp, &Kernel.*/2) |> execute(sp + 4)
+      3 -> machine |> write_input(sp) |> execute(sp + 2)
+      4 -> machine |> output(sp) |> execute(sp + 2)
+      99 -> machine
     end
   end
 
-  defp op(opcodes, i, f) do
-    a = Map.get(opcodes, i + 1)
-    b = Map.get(opcodes, i + 2)
-    c = Map.get(opcodes, i + 3)
-    val_a = Map.get(opcodes, a)
-    val_b = Map.get(opcodes, b)
-    Map.put(opcodes, c, f.(val_a, val_b))
+  defp op(machine, sp, f) do
+    a = read(machine, sp + 1)
+    b = read(machine, sp + 2)
+    c = read(machine, sp + 3)
+    val_a = read(machine, a)
+    val_b = read(machine, b)
+    write(machine, c, f.(val_a, val_b))
   end
 
-  defp write_input(opcodes, i) do
-    addr = read(opcodes, i + 1)
-    write(opcodes, addr, input())
+  defp write_input(machine, sp) do
+    addr = read(machine, sp + 1)
+    write(machine, addr, input())
   end
 
   defp input, do: 1
 
-  defp output(opcodes, i) do
-    addr = read(opcodes, i + 1)
-    IO.puts read(opcodes, addr)
-    opcodes
+  defp output(machine, sp) do
+    addr = read(machine, sp + 1)
+    IO.puts read(machine, addr)
+    machine
   end
 
   def read(machine, addr), do: Map.get(machine, addr)
   def write(machine, addr, value), do: Map.put(machine, addr, value)
 
   def assign_offsets([], _, map), do: map
-  def assign_offsets([op | rest], i, map) do
-    assign_offsets(rest, i + 1, write(map, i, op))
+  def assign_offsets([op | rest], sp, map) do
+    assign_offsets(rest, sp + 1, write(map, sp, op))
   end
 
   defp divrem(a, b), do: {div(a, b), rem(a, b)}
