2a6d3e8d4a9f8bc2f191a7903893c8217193f0d5
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-12-06 18:58:09 -0600

    Accept arbitrary input when running diagnostics

--- year_2019/lib/day_05/dianostics.ex
+++ year_2019/lib/day_05/dianostics.ex
@@ -1,6 +1,7 @@
 defmodule Day05.Diagnostics do
-  def run do
+  def run(input) do
     {:ok, raw} = File.read("#{__DIR__}/input.txt")
-    raw |> Intcode.build |> Intcode.execute
+    raw |> Intcode.build |> Intcode.execute(input)
+    :ok
   end
 end
