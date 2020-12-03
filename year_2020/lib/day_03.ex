15b7ae8608257774309aeabfd67b4944da1ae9fe
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-03 09:25:51 -0600

    Day 3, Part 2

--- year_2020/lib/day_03.ex
+++ year_2020/lib/day_03.ex
@@ -1,15 +1,25 @@
 defmodule Day03 do
   def part_one do
-    count(lines(), 0, 0)
+    count(lines(), 0, 0, {3, 1})
   end
 
-  defp count([], total, _index), do: total
-  defp count([line | rest], total, index) do
-    next_index = rem(index + 3, String.length(line))
+  def part_two do
+    rows = lines()
+    [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+    |> Enum.map(&(count(rows, 0, 0, &1)))
+    |> Enum.reduce(fn(a, b) -> a * b end)
+  end
+
+  defp count([], total, _index, _slope), do: total
+  defp count([_line], total, _index, {_right, 2}), do: total
+  defp count([line | rest], total, index, {_right, 1} = slope), do: count(line, rest, total, index, slope)
+  defp count([line | [_skip | rest]], total, index, {_right, 2} = slope), do: count(line, rest, total, index, slope)
+  defp count(line, rest, total, index, {right, _down} = slope) do
+    next_index = rem(index + right, String.length(line))
     if Day03.Forest.tree?(line, index) do
-      count(rest, total + 1, next_index)
+      count(rest, total + 1, next_index, slope)
     else
-      count(rest, total, next_index)
+      count(rest, total, next_index, slope)
     end
   end
 
