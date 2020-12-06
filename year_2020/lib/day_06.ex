075dc40689171cfbbdc9f49c9920ddf4883c7fc5
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-06 09:37:05 -0600

    Day 6, Part 2
    Rewrite Part 1 to use a unified approach

--- year_2020/lib/day_06.ex
+++ year_2020/lib/day_06.ex
@@ -1,16 +1,26 @@
 defmodule Day06 do
   def part_one do
+    count(&MapSet.union/2)
+  end
+
+  def part_two do
+    count(&MapSet.intersection/2)
+  end
+
+  defp count(reducer) do
     InputFile.contents_of(6, :stream)
     |> Enum.map(&String.trim/1)
-    |> Enum.chunk_while("", &accum/2, &finish/1)
-    |> Enum.map(fn str -> String.split(str, "", trim: true) end)
-    |> Enum.map(&Enum.uniq/1)
+    |> Enum.chunk_while([], &accum/2, &finish/1)
+    |> Enum.map(fn sets -> Enum.reduce(sets, fn a, b -> reducer.(a, b) end) end)
     |> Enum.map(&Enum.count/1)
     |> Enum.reduce(fn a, b -> a + b end)
   end
 
-  defp accum("", str), do: {:cont, str, ""}
-  defp accum(line, str), do: {:cont, "#{str}#{line}"}
+  defp accum("", sets), do: {:cont, sets, []}
+  defp accum(line, sets) do
+    set = line |> String.split("", trim: true) |> Enum.into(MapSet.new())
+    {:cont, [set | sets]}
+  end
 
-  defp finish(str), do: {:cont, str, ""}
+  defp finish(set), do: {:cont, set, []}
 end
