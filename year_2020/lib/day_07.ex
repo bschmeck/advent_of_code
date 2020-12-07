5a15f84132d090508a9b8b81a85a6218e6458bad
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-07 08:28:45 -0600

    Day 7, Part 1

---
+++ year_2020/lib/day_07.ex
@@ -0,0 +1,43 @@
+defmodule Day07 do
+  def part_one do
+    InputFile.contents_of(7, :stream)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&parse/1)
+    |> Enum.map(&invert/1)
+    |> Enum.reduce(fn a, b -> Map.merge(a, b, fn _k, c, d -> c ++ d end) end)
+    |> containers(["shiny gold"], MapSet.new())
+    |> Enum.count
+  end
+
+  def parse(line) do
+    ~r/^(?<color>.*) bags contain (?<contents>.*)\.$/
+    |> Regex.named_captures(line)
+    |> captures_to_rule()
+  end
+
+  def invert({color, contents}), do: invert(color, contents, %{})
+  def invert(_color, [], map), do: map
+  def invert(color, [content | rest], map), do: invert(color, rest, Map.put(map, content, [color]))
+
+  def containers(_map, [], seen), do: seen
+  def containers(map, colors, seen) do
+    new = colors
+    |> Enum.flat_map(&(Map.get(map, &1, [])))
+    |> Enum.reject(&(MapSet.member?(seen, &1)))
+
+    containers(map, new, Enum.into(new, seen))
+  end
+
+  defp captures_to_rule(%{"color" => color, "contents" => "no other bags"}), do: {color, []}
+  defp captures_to_rule(%{"color" => color, "contents" => contents}), do: {color, contents |> String.split(",") |> extract_contents()}
+
+  defp extract_contents(raw), do: extract_contents(raw, [])
+  defp extract_contents([], contents), do: contents
+  defp extract_contents([str | rest], contents) do
+    color = ~r/\d+ (?<color>.*) bags?$/
+    |> Regex.named_captures(String.trim(str))
+    |> Map.get("color")
+
+    extract_contents(rest, [color | contents])
+  end
+end
