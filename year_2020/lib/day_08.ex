9d87c4ad2f03447ad5bc1b561cd536a8e7b799f1
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-08 10:27:37 -0600

    Flip jmps and nops until we find a working program

--- year_2020/lib/day_08.ex
+++ year_2020/lib/day_08.ex
@@ -7,6 +7,36 @@ defmodule Day08 do
     |> Day08.TapeMachine.execute()
   end
 
+  def part_two(file_reader \\ InputFile) do
+    file_reader.contents_of(8, :stream)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&parse/1)
+    |> mutate()
+  end
+
   defp parse(<<opcode :: binary-size(3), " +", value :: binary>>), do: {opcode, String.to_integer(value)}
   defp parse(<<opcode :: binary-size(3), " -", value :: binary>>), do: {opcode, -String.to_integer(value)}
+
+  defp mutate(instructions), do: mutate([], instructions)
+  defp mutate(prev, [{"nop", val} | rest]) do
+    prev
+    |> Kernel.++([{"jmp", val} | rest])
+    |> Day08.TapeMachine.new()
+    |> Day08.TapeMachine.execute()
+    |> case do
+        {:ok, accum} -> {:ok, accum}
+        _ -> mutate(prev ++ [{"nop", val}], rest)
+    end
+  end
+  defp mutate(prev, [{"jmp", val} | rest]) do
+    prev
+    |> Kernel.++([{"nop", val} | rest])
+    |> Day08.TapeMachine.new()
+    |> Day08.TapeMachine.execute()
+    |> case do
+        {:ok, accum} -> {:ok, accum}
+        _ -> mutate(prev ++ [{"jmp", val}], rest)
+    end
+  end
+  defp mutate(prev, [instr | rest]), do: mutate(prev ++ [instr], rest)
 end
--- year_2020/test/day_08_test.exs
+++ year_2020/test/day_08_test.exs
@@ -4,4 +4,8 @@ defmodule Day08Test do
   test "it detects loops" do
     assert {:loop, 5} = Day08.part_one(InputTestFile)
   end
+
+  test "it mutates programs to find valid ones" do
+    assert {:ok, 8} = Day08.part_two(InputTestFile)
+  end
 end
