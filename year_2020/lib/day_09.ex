a1c9147aab93f8a8dc6f82c0988f6374d35f2a62
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-09 07:30:56 -0600

    Day 9, Part 2

--- year_2020/lib/day_09.ex
+++ year_2020/lib/day_09.ex
@@ -1,10 +1,24 @@
 defmodule Day09 do
   def part_one(file_reader \\ InputFile, preamble_len \\ 25) do
+    stream(file_reader)
+    |> Enum.split(preamble_len)
+    |> find_weakness()
+  end
+
+  def part_two(file_reader \\ InputFile, preamble_len \\ 25) do
+    stream = stream(file_reader)
+
+    weakness = stream
+    |> Enum.split(preamble_len)
+    |> find_weakness()
+
+    find_encryption_weakness(stream, weakness)
+  end
+
+  defp stream(file_reader) do
     file_reader.contents_of(9, :stream)
     |> Enum.map(&String.trim/1)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.split(preamble_len)
-    |> find_weakness()
   end
 
   defp find_weakness({stream, [n | rest]}) do
@@ -17,4 +31,16 @@ defmodule Day09 do
         _ -> n
     end
   end
+
+  defp find_encryption_weakness(stream, target) do
+    case find_encryption_weakness_seq(stream, target, []) do
+      nil -> find_encryption_weakness(tl(stream), target)
+      arr ->
+        {a, b} = Enum.min_max(arr)
+        a + b
+    end
+  end
+  defp find_encryption_weakness_seq(_stream, 0, sequence), do: sequence
+  defp find_encryption_weakness_seq([n | _rest], target, _sequence) when n > target, do: nil
+  defp find_encryption_weakness_seq([n | rest], target, sequence), do: find_encryption_weakness_seq(rest, target - n, [n | sequence])
 end
--- year_2020/test/day_09_test.exs
+++ year_2020/test/day_09_test.exs
@@ -4,4 +4,8 @@ defmodule Day09Test do
   test "it finds the weakness" do
     assert Day09.part_one(InputTestFile, 5) == 127
   end
+
+  test "it finds the encryption weakness" do
+    assert Day09.part_two(InputTestFile, 5) == 62
+  end
 end
