5f1e27d58c6f39a781edd3c57225d8fe12806260
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-11 10:37:47 -0600

    Parameterize and consolidate the stabilization approaches

--- year_2020/lib/day_11.ex
+++ year_2020/lib/day_11.ex
@@ -3,7 +3,7 @@ defmodule Day11 do
     file_reader.contents_of(11, :stream)
     |> Enum.map(&String.trim/1)
     |> Day11.Seats.new()
-    |> Day11.Seats.stabilize(&advance_v1/1)
+    |> Day11.Seats.stabilize(ignore_floor: false, threshold: 4)
     |> Day11.Seats.count_empty_seats()
   end
 
@@ -11,91 +11,8 @@ defmodule Day11 do
     file_reader.contents_of(11, :stream)
     |> Enum.map(&String.trim/1)
     |> Day11.Seats.new()
-    |> Day11.Seats.stabilize(&advance_v2/1)
+    |> Day11.Seats.stabilize(ignore_floor: true, threshold: 5)
     |> Day11.Seats.count_empty_seats()
-  end
-
-  def advance_v1(grid) do
-    new_vals = Enum.map(grid, fn entry -> new_value_v1(entry, grid) end)
-
-    outcome = Enum.any?(new_vals, fn {_key, _new_val, result} -> result == :change end)
-    |> case do
-        true -> :change
-        false -> :stable
-      end
-
-    {outcome, Enum.into(new_vals, %{}, fn {key, val, _result} -> {key, val} end)}
-  end
-
-  def new_value_v1({pos, "."}, _grid), do: {pos, ".", :stable}
-  def new_value_v1({pos, "L"}, grid) do
-    pos
-    |> adjacent_v1(grid)
-    |> Enum.count(fn seat -> seat == "#" end)
-    |> case do
-        0 -> {pos, "#", :change}
-        _ -> {pos, "L", :stable}
-    end
-  end
-  def new_value_v1({pos, "#"}, grid) do
-    pos
-    |> adjacent_v1(grid)
-    |> Enum.count(fn seat -> seat == "#" end)
-    |> case do
-        x when x >= 4 -> {pos, "L", :change}
-        _ -> {pos, "#", :stable}
-    end
-  end
-
-  def adjacent_v1({x, y}, grid) do
-    [{-1, -1}, {0, -1}, {1, -1},{-1, 0}, {1, 0},{-1, 1}, {0, 1}, {1, 1}]
-    |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
-    |> Enum.map(fn pos -> Map.get(grid, pos, "") end)
-  end
-
-  def advance_v2(grid) do
-    new_vals = Enum.map(grid, fn entry -> new_value_v2(entry, grid) end)
-
-    outcome = Enum.any?(new_vals, fn {_key, _new_val, result} -> result == :change end)
-    |> case do
-        true -> :change
-        false -> :stable
-      end
-
-    {outcome, Enum.into(new_vals, %{}, fn {key, val, _result} -> {key, val} end)}
-  end
-
-  def new_value_v2({pos, "."}, _grid), do: {pos, ".", :stable}
-  def new_value_v2({pos, "L"}, grid) do
-    pos
-    |> adjacent_v2(grid)
-    |> Enum.count(fn seat -> seat == "#" end)
-    |> case do
-        0 -> {pos, "#", :change}
-        _ -> {pos, "L", :stable}
-    end
-  end
-  def new_value_v2({pos, "#"}, grid) do
-    pos
-    |> adjacent_v2(grid)
-    |> Enum.count(fn seat -> seat == "#" end)
-    |> case do
-        x when x >= 5 -> {pos, "L", :change}
-        _ -> {pos, "#", :stable}
-    end
-  end
-
-  def adjacent_v2({x, y}, grid) do
-    [{-1, -1}, {0, -1}, {1, -1},{-1, 0}, {1, 0},{-1, 1}, {0, 1}, {1, 1}]
-    |> Enum.map(&(seat_in_dir(&1, {x, y}, 1, grid)))
-  end
-
-  def seat_in_dir({dx, dy}, {x, y}, i, grid) do
-    pos = {x + dx * i, y + dy * i}
-    case Map.get(grid, pos, "L") do
-      "." -> seat_in_dir({dx, dy}, {x, y}, i + 1, grid)
-      seat -> seat
-    end
   end
 
   def print(grid) do
--- year_2020/lib/day_11/seats.ex
+++ year_2020/lib/day_11/seats.ex
@@ -3,10 +3,10 @@ defmodule Day11.Seats do
 
   def new(input), do: %__MODULE__{grid: gridify(input, 0, %{}) }
 
-  def stabilize(%__MODULE__{grid: grid}, advancer) do
-    case advancer.(grid) do
+  def stabilize(%__MODULE__{grid: grid}, opts) do
+    case advance(grid, opts) do
       {:stable, grid} -> %__MODULE__{grid: grid}
-      {:change, grid} -> stabilize(%__MODULE__{grid: grid}, advancer)
+      {:change, grid} -> stabilize(%__MODULE__{grid: grid}, opts)
     end
   end
 
@@ -24,5 +24,54 @@ defmodule Day11.Seats do
     |> Enum.into(grid, fn {char, col} -> {{row, col}, char} end)
 
     gridify(rest, row + 1, grid)
+  end
+
+  defp advance(grid, opts) do
+    new_vals = Enum.map(grid, fn entry -> new_value(entry, grid, opts) end)
+
+    outcome = Enum.any?(new_vals, fn {_key, _new_val, result} -> result == :change end)
+    |> case do
+        true -> :change
+        false -> :stable
+      end
+
+    {outcome, Enum.into(new_vals, %{}, fn {key, val, _result} -> {key, val} end)}
+  end
+
+  def new_value({pos, "."}, _grid, _opts), do: {pos, ".", :stable}
+  def new_value({pos, "L"}, grid, ignore_floor: ignore_floor, threshold: _) do
+    pos
+    |> adjacent(grid, ignore_floor)
+    |> Enum.count(fn seat -> seat == "#" end)
+    |> case do
+        0 -> {pos, "#", :change}
+        _ -> {pos, "L", :stable}
+    end
+  end
+  def new_value({pos, "#"}, grid, ignore_floor: ignore_floor, threshold: threshold) do
+    pos
+    |> adjacent(grid, ignore_floor)
+    |> Enum.count(fn seat -> seat == "#" end)
+    |> case do
+        x when x >= threshold -> {pos, "L", :change}
+        _ -> {pos, "#", :stable}
+    end
+  end
+
+  def adjacent({x, y}, grid, ignore_floor) do
+    [{-1, -1}, {0, -1}, {1, -1},{-1, 0}, {1, 0},{-1, 1}, {0, 1}, {1, 1}]
+    |> Enum.map(&(seat_in_dir(&1, {x, y}, 1, grid, ignore_floor)))
+  end
+
+  def seat_in_dir({dx, dy}, {x, y}, i, grid, false) do
+    pos = {x + dx * i, y + dy * i}
+    Map.get(grid, pos, "L")
+  end
+  def seat_in_dir({dx, dy}, {x, y}, i, grid, true) do
+    pos = {x + dx * i, y + dy * i}
+    case Map.get(grid, pos, "L") do
+      "." -> seat_in_dir({dx, dy}, {x, y}, i + 1, grid, true)
+      seat -> seat
+    end
   end
 end
