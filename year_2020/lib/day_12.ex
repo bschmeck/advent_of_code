c2a34219cf71156c6d9d086e74f58d9ff9fdf8e3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-12 08:38:40 -0600

    mix format

--- year_2020/lib/day_12.ex
+++ year_2020/lib/day_12.ex
@@ -1,16 +1,16 @@
 defmodule Day12 do
-
   def part_one(file_reader \\ InputFile), do: navigate(file_reader, Day12.Ship)
   def part_two(file_reader \\ InputFile), do: navigate(file_reader, Day12.WaypointShip)
 
   def navigate(file_reader, ship_type) do
-    ship = file_reader.contents_of(12, :stream)
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&parse/1)
-    |> ship_type.follow_instructions()
+    ship =
+      file_reader.contents_of(12, :stream)
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&parse/1)
+      |> ship_type.follow_instructions()
 
     abs(ship.x) + abs(ship.y)
   end
 
-  def parse(<<op :: binary-size(1), rest :: binary>>), do: {op, String.to_integer(rest)}
+  def parse(<<op::binary-size(1), rest::binary>>), do: {op, String.to_integer(rest)}
 end
--- year_2020/lib/day_12/waypoint_ship.ex
+++ year_2020/lib/day_12/waypoint_ship.ex
@@ -1,9 +1,11 @@
 defmodule Day12.WaypointShip do
-  defstruct [x: 0, y: 0, wx: 10, wy: 1]
+  defstruct x: 0, y: 0, wx: 10, wy: 1
 
   def follow_instructions(instrs), do: follow_instructions(instrs, %__MODULE__{})
   def follow_instructions([], ship), do: ship
-  def follow_instructions([instr | rest], ship), do: follow_instructions(rest, follow_instruction(instr, ship))
+
+  def follow_instructions([instr | rest], ship),
+    do: follow_instructions(rest, follow_instruction(instr, ship))
 
   def follow_instruction({"N", v}, ship), do: %__MODULE__{ship | wy: ship.wy + v}
   def follow_instruction({"S", v}, ship), do: %__MODULE__{ship | wy: ship.wy - v}
@@ -11,7 +13,9 @@ defmodule Day12.WaypointShip do
   def follow_instruction({"W", v}, ship), do: %__MODULE__{ship | wx: ship.wx - v}
   def follow_instruction({"L", v}, ship), do: rotate(ship, 360 - v)
   def follow_instruction({"R", v}, ship), do: rotate(ship, v)
-  def follow_instruction({"F", v}, ship), do: %__MODULE__{ship | x: ship.x + v * ship.wx, y: ship.y + v * ship.wy}
+
+  def follow_instruction({"F", v}, ship),
+    do: %__MODULE__{ship | x: ship.x + v * ship.wx, y: ship.y + v * ship.wy}
 
   def rotate(ship, 90), do: %__MODULE__{ship | wx: ship.wy, wy: -ship.wx}
   def rotate(ship, 180), do: ship |> rotate(90) |> rotate(90)
