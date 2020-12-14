3219d896e909659f0e7182ad958aa6e50552e68a
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-14 00:32:25 -0600

    Day 13, Part 2

--- year_2020/lib/day_13.ex
+++ year_2020/lib/day_13.ex
@@ -15,6 +15,56 @@ defmodule Day13 do
     |> Enum.reduce(&Kernel.*/2)
   end
 
+  def part_two(file_reader \\ InputFile) do
+    [_timestamp, shuttles, ""] =
+      file_reader.contents_of(13)
+      |> String.split("\n")
+
+    shuttles
+    |> String.split(",")
+    |> Enum.map(fn
+      "x" -> -1
+      c -> String.to_integer(c)
+    end)
+    |> Enum.with_index()
+    |> Enum.reject(fn {x, _i} -> x == -1 end)
+    |> compute_arrival
+  end
+
+  def compute_arrival(shuttles) do
+    big_m = shuttles |> Enum.map(fn {x, _i} -> x end) |> Enum.reduce(&Kernel.*/2)
+    # The head will always be zeroed out, so we just drop it from the calc
+    sum =
+      shuttles
+      |> tl
+      |> Enum.map(fn {x, i} ->
+        m = div(big_m, x)
+
+        (x - i) * m * modular_inverse(rem(m, x), x)
+      end)
+      |> Enum.reduce(&Kernel.+/2)
+
+    rem(sum, big_m)
+  end
+
+  def gcd(a, b) when a > b, do: gcd(a, b, [])
+  def gcd(a, b), do: gcd(b, a)
+  def gcd(_a, 0, divs), do: divs |> tl |> Enum.reverse()
+  def gcd(0, _b, divs), do: divs |> tl |> Enum.reverse()
+  def gcd(a, b, divs), do: gcd(b, rem(a, b), [div(a, b) | divs])
+
+  def modular_inverse(a, b) do
+    gcd(a, b) |> iter(0, 1, b)
+  end
+
+  def iter(qs, p_0, p_1, b) when p_1 < 0, do: iter(qs, p_0, b + p_1, b)
+  def iter([], _, p_1, _), do: p_1
+
+  def iter([q | rest], p_0, p_1, b) do
+    p_2 = rem(p_0 - p_1 * q, b)
+    iter(rest, p_1, p_2, b)
+  end
+
   defp time_to_shuttle(timestamp, t) when rem(timestamp, t) == 0, do: 0
   defp time_to_shuttle(timestamp, t), do: t - rem(timestamp, t)
 end
--- year_2020/test/day_13_test.exs
+++ year_2020/test/day_13_test.exs
@@ -4,4 +4,17 @@ defmodule Day13Test do
   test "it computes time to the next shuttle" do
     assert Day13.part_one(InputTestFile) == 295
   end
+
+  test "it computes the right timestamp" do
+    assert Day13.part_two(InputTestFile) == 1_068_781
+  end
+
+  test "it computes factors used by GCD" do
+    assert Day13.gcd(15, 26) == [1, 1, 2, 1]
+  end
+
+  test "it computes the modular inverse" do
+    assert Day13.modular_inverse(11, 13) == 6
+    assert Day13.modular_inverse(12, 19) == 8
+  end
 end
