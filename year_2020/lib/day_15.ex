b416781a58160105f0400ca741d4f6803770ffc4
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-16 11:21:28 -0600

    mix format

--- year_2020/lib/day_15.ex
+++ year_2020/lib/day_15.ex
@@ -1,22 +1,26 @@
 defmodule Day15 do
-  def part_one(numbers \\ [12,20,0,6,1,17,7]) do
+  def part_one(numbers \\ [12, 20, 0, 6, 1, 17, 7]) do
     play(numbers, 2020)
   end
 
-  def part_two(numbers \\ [12,20,0,6,1,17,7]) do
-    play(numbers,3_0000_000 )
+  def part_two(numbers \\ [12, 20, 0, 6, 1, 17, 7]) do
+    play(numbers, 30_0000_000)
   end
 
   def play([speak | rest], goal), do: play(rest, 1, speak, %{}, goal)
   def play(_, turn, speak, _spoken, goal) when goal == turn, do: speak
+
   def play([], turn, speak, spoken, goal) do
-    next_speak = case Map.get(spoken, speak, 0) do
-                   0 -> 0
-                   n -> turn - n
-                 end
+    next_speak =
+      case Map.get(spoken, speak, 0) do
+        0 -> 0
+        n -> turn - n
+      end
+
     spoken = Map.put(spoken, speak, turn)
     play([], turn + 1, next_speak, spoken, goal)
   end
+
   def play([next_speak | rest], turn, speak, spoken, goal) do
     play(rest, turn + 1, next_speak, Map.put(spoken, speak, turn), goal)
   end
