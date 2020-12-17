5f64fe9824760971fb4935d749c5c4e1f375c4ee
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-17 10:02:49 -0600

    Extract simulation code in advance of part two

--- year_2020/lib/day_17.ex
+++ year_2020/lib/day_17.ex
@@ -1,16 +1,20 @@
 defmodule Day17 do
   def part_one(file_reader \\ InputFile) do
+    perform(file_reader, Day17.PocketDimension)
+  end
+
+  def perform(file_reader, dimension_mod) do
     final = file_reader.contents_of(17)
-    |> Day17.PocketDimension.parse()
-    |> run_simulation(6)
+    |> dimension_mod.parse()
+    |> run_simulation(6, &dimension_mod.simulate/1)
 
     Enum.count(final.grid)
   end
 
-  def run_simulation(dim, 0), do: dim
-  def run_simulation(dim, n) do
+  def run_simulation(dim, 0, _simulator), do: dim
+  def run_simulation(dim, n, simulator) do
     dim
-    |> Day17.PocketDimension.simulate()
-    |> run_simulation(n - 1)
+    |> simulator.()
+    |> run_simulation(n - 1, simulator)
   end
 end
