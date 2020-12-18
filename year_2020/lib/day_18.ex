e90cdf5f4dc490c40fc7a27bdb80e1f2339fb936
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-18 09:11:20 -0600

    Day 18, Part 1

---
+++ year_2020/lib/day_18.ex
@@ -0,0 +1,8 @@
+defmodule Day18 do
+  def part_one(file_reader \\ InputFile) do
+    file_reader.contents_of(18, :stream)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&Day18.Calculator.eval/1)
+    |> Enum.reduce(&Kernel.+/2)
+  end
+end
