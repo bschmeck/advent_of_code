ce241d73945c65dd462fea118a8df62f3fbbd203
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-18 08:07:32 -0600

    Handle parens

--- year_2020/lib/day_18/calculator.ex
+++ year_2020/lib/day_18/calculator.ex
@@ -1,9 +1,26 @@
 defmodule Day18.Calculator do
-  def eval(str), do: eval(String.split(str, " "), [])
+  def eval(str) do
+    str
+    |> String.split(" ")
+    |> Enum.flat_map(&parse/1)
+    |> eval([])
+  end
 
-  def eval([v | rest], []), do: eval(rest, [String.to_integer(v)])
+  def eval(["(" | rest], stack), do: eval(rest, ["(" | stack])
+  def eval([")" | rest], [v | ["(" | stack]]), do: eval([v | rest], stack)
   def eval(["+" | rest], stack), do: eval(rest, [&Kernel.+/2 | stack])
   def eval(["*" | rest], stack), do: eval(rest, [&Kernel.*/2 | stack])
-  def eval([v | rest], [f | [n | stack]]) when is_function(f), do: eval(rest, [f.(String.to_integer(v), n) | stack])
+  def eval([v | rest], [f | [n | stack]]) when is_function(f), do: eval(rest, [f.(v, n) | stack])
+  def eval([v | rest], stack), do: eval(rest, [v | stack])
   def eval([], [result]), do: result
+
+  def parse("+"), do: ["+"]
+  def parse("*"), do: ["*"]
+  def parse(<<"(", rest :: binary>>), do: ["("] ++ parse(rest)
+  def parse(v) do
+    case Integer.parse(v) do
+      {n, ""} -> [n]
+      {n, parens} -> [n | String.split(parens, "", trim: true)]
+    end
+  end
 end
--- year_2020/test/day_18/calculator_test.exs
+++ year_2020/test/day_18/calculator_test.exs
@@ -12,4 +12,19 @@ defmodule Day18.CalculatorTest do
   test "it obeys order of operations" do
     assert Day18.Calculator.eval("1 + 2 * 3") == 9
   end
+
+  test "it supports parenthenses" do
+    assert Day18.Calculator.eval("(2 + 3)") == 5
+  end
+
+  test "it obeys order of operations with parens" do
+    assert Day18.Calculator.eval("1 + (2 * 3)") == 7
+  end
+
+  test "the AoC examples work" do
+    assert Day18.Calculator.eval("2 * 3 + (4 * 5)") == 26
+    assert Day18.Calculator.eval("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
+    assert Day18.Calculator.eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
+    assert Day18.Calculator.eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
+  end
 end
