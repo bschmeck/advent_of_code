cafe17ba72094890e66ae2c10ef054981d9b5f9b
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-12-21 09:25:36 -0600

    Day 21, Part 2

--- year_2020/lib/day_21.ex
+++ year_2020/lib/day_21.ex
@@ -19,4 +19,33 @@ defmodule Day21 do
     |> Enum.reject(&(MapSet.member?(possibles, &1)))
     |> Enum.count
   end
+
+  def part_two(file_reader \\ InputFile) do
+    file_reader.contents_of(21, :stream)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&Day21.IngredientList.parse/1)
+    |> Enum.flat_map(fn list ->
+      Enum.map(list.allergens, &({&1, list.ingredients}))
+    end)
+    |> Enum.reduce(%{}, fn {allergen, ingredients}, possibles ->
+      Map.update(possibles, allergen, MapSet.new(ingredients), fn set -> MapSet.intersection(set, MapSet.new(ingredients)) end)
+    end)
+    |> eliminate()
+    |> Enum.sort()
+    |> Enum.map(fn {_allergen, ingredient} -> ingredient end)
+    |> Enum.join(",")
+  end
+
+  def eliminate(possibles), do: eliminate(possibles, %{})
+  def eliminate(possibles, mapping) when possibles == %{}, do: mapping
+  def eliminate(possibles, mapping) do
+    {allergen, ingredient} = Enum.find(possibles, fn {_k, v} -> MapSet.size(v) == 1 end)
+    ingredient = ingredient |> MapSet.to_list() |> hd()
+
+    possibles
+    |> Map.keys()
+    |> Enum.reduce(possibles, fn k, p -> Map.update!(p, k, &(MapSet.delete(&1, ingredient))) end)
+    |> Map.delete(allergen)
+    |> eliminate(Map.put(mapping, allergen, ingredient))
+  end
 end
--- year_2020/test/day_21_test.exs
+++ year_2020/test/day_21_test.exs
@@ -4,4 +4,8 @@ defmodule Day21Test do
   test "it counts non-allergic ingredients" do
     assert Day21.part_one(InputTestFile) == 5
   end
+
+  test "it computes a dangerous ingredient list" do
+    assert Day21.part_two(InputTestFile) == "mxmxvkd,sqjhc,fvjkl"
+  end
 end
