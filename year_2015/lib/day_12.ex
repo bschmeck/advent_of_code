6011c8565fbe2f079c8a05be3cc54d47f81f9863
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-01-11 16:12:03 -0600

    Day 12, reworked to actually parse JSON

--- year_2015/lib/day_12.ex
+++ year_2015/lib/day_12.ex
@@ -1,27 +1,34 @@
 defmodule Day12 do
   def part_one(file_reader \\ InputFile) do
     file_reader.contents_of(12)
-    |> String.split("", trim: true)
-    |> Enum.map(&to_integer/1)
-    |> Enum.chunk_while({0, 1}, &chunk_fun/2, &after_fun/1)
+    |> Jason.decode!()
+    |> Enum.map(&extract_numbers/1)
     |> Enum.reduce(&Kernel.+/2)
   end
 
-  def chunk_fun(nil, {total, sign}), do: {:cont, sign * total, {0, 1}}
-  def chunk_fun("-", {0, 1}), do: {:cont, {0, -1}}
-  def chunk_fun(digit, {total, sign}), do: {:cont, {10 * total + digit, sign}}
-  def after_fun({total, sign}), do: {:cont, sign * total, {0, 1}}
+  def part_two(file_reader \\ InputFile) do
+    file_reader.contents_of(12)
+    |> Jason.decode!()
+    |> Enum.map(&extract_non_red_numbers/1)
+    |> Enum.reduce(&Kernel.+/2)
+  end
 
-  def to_integer("1"), do: 1
-  def to_integer("2"), do: 2
-  def to_integer("3"), do: 3
-  def to_integer("4"), do: 4
-  def to_integer("5"), do: 5
-  def to_integer("6"), do: 6
-  def to_integer("7"), do: 7
-  def to_integer("8"), do: 8
-  def to_integer("9"), do: 9
-  def to_integer("0"), do: 0
-  def to_integer("-"), do: "-"
-  def to_integer(_), do: nil
+  def extract_numbers(n) when is_integer(n), do: n
+  def extract_numbers(list) when is_list(list), do: list |> Enum.map(&extract_numbers/1) |> Enum.reduce(&Kernel.+/2)
+  def extract_numbers(map) when is_map(map), do: map |> Map.values() |> extract_numbers()
+  def extract_numbers(_), do: 0
+
+  def extract_non_red_numbers(n) when is_integer(n), do: n
+  def extract_non_red_numbers(list) when is_list(list), do: list |> Enum.map(&extract_non_red_numbers/1) |> Enum.reduce(&Kernel.+/2)
+  def extract_non_red_numbers(map) when is_map(map) do
+    map
+    |> Map.values()
+    |> MapSet.new()
+    |> MapSet.member?("red")
+    |> case do
+      true -> 0
+      false -> extract_non_red_numbers(Map.values(map))
+    end
+  end
+  def extract_non_red_numbers(_), do: 0
 end
