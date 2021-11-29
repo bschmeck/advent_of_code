6ad27587b498f26bd36d274a8b0c43781a7ad816
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-11-29 13:53:43 -0600

    Prep for 2021

---
+++ year_2021/lib/year2021.ex
@@ -0,0 +1,18 @@
+defmodule Year2021 do
+  @moduledoc """
+  Documentation for `Year2021`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Year2021.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ year_2021/test/year2021_test.exs
@@ -0,0 +1,8 @@
+defmodule Year2021Test do
+  use ExUnit.Case
+  doctest Year2021
+
+  test "greets the world" do
+    assert Year2021.hello() == :world
+  end
+end
