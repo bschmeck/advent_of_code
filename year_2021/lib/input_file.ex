38d6f96d92d066e3a4efd969e4d180a398854d64
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-11-29 13:57:53 -0600

    Create a copy of InputFile and InputTestFile for 2021

---
+++ year_2021/lib/input_file.ex
@@ -0,0 +1,18 @@
+defmodule InputFile do
+  def filename_for(n) do
+    day = n |> Integer.to_string() |> String.pad_leading(2, "0")
+    Path.join(:code.priv_dir(:year_2021), "day_#{day}.txt")
+  end
+
+  def contents_of(n, mode \\ :read)
+
+  def contents_of(n, :stream) do
+    filename_for(n)
+    |> File.stream!([], :line)
+  end
+
+  def contents_of(n, :read) do
+    {:ok, raw} = filename_for(n) |> File.read()
+    raw
+  end
+end
