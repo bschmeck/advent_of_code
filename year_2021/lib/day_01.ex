969103dbb9b46c88310c038171721aa989d20527
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-12-01 09:49:20 -0600

    Refactoring

--- year_2021/lib/day_01.ex
+++ year_2021/lib/day_01.ex
@@ -1,21 +1,33 @@
 defmodule Day01 do
   def part_one(module) do
+    module
+    |> measurements()
+    |> count_increasing()
+  end
+
+  def part_two(module) do
+    module
+    |> measurements()
+    |> sliding_window(3)
+    |> count_increasing()
+  end
+
+  defp measurements(module) do
     module.contents_of(1, :stream)
     |> Stream.map(&String.trim_trailing(&1))
     |> Stream.map(fn l -> l |> Integer.parse() |> elem(0) end)
+  end
+
+  defp count_increasing(stream) do
+    stream
     |> Stream.chunk_every(2, 1, :discard)
     |> Stream.filter(fn [a, b] -> a < b end)
     |> Enum.count()
   end
 
-  def part_two(module) do
-    module.contents_of(1, :stream)
-    |> Stream.map(&String.trim_trailing(&1))
-    |> Stream.map(fn l -> l |> Integer.parse() |> elem(0) end)
-    |> Stream.chunk_every(3, 1, :discard)
+  defp sliding_window(stream, size) do
+    stream
+    |> Stream.chunk_every(size, 1, :discard)
     |> Stream.map(&Enum.sum(&1))
-    |> Stream.chunk_every(2, 1, :discard)
-    |> Stream.filter(fn [a, b] -> a < b end)
-    |> Enum.count()
   end
 end
