85710e786cd8760ad2245da24250f57aa0bc35c0
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-12-01 09:46:23 -0600

    Day 1, Part 2 solution

--- year_2021/lib/day_01.ex
+++ year_2021/lib/day_01.ex
@@ -7,4 +7,15 @@ defmodule Day01 do
     |> Stream.filter(fn [a, b] -> a < b end)
     |> Enum.count()
   end
+
+  def part_two(module) do
+    module.contents_of(1, :stream)
+    |> Stream.map(&String.trim_trailing(&1))
+    |> Stream.map(fn l -> l |> Integer.parse() |> elem(0) end)
+    |> Stream.chunk_every(3, 1, :discard)
+    |> Stream.map(&Enum.sum(&1))
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Stream.filter(fn [a, b] -> a < b end)
+    |> Enum.count()
+  end
 end
--- year_2021/test/day_01_test.exs
+++ year_2021/test/day_01_test.exs
@@ -4,4 +4,8 @@ defmodule Day01Test do
   test "it counts increasing depth measurements" do
     assert Day01.part_one(InputTestFile) == 7
   end
+
+  test "it counts the number of increasing sliding windows" do
+    assert Day01.part_two(InputTestFile) == 5
+  end
 end
