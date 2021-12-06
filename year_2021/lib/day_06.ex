06b61ecd2f00609a1c7b3aabc05ab99b8047338d
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-12-06 08:11:15 -0600

    Don't simulate every fish, just track the counts per interval

--- year_2021/lib/day_06.ex
+++ year_2021/lib/day_06.ex
@@ -5,12 +5,22 @@ defmodule Day06 do
     |> String.trim()
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
-    |> do_simulate(days, [])
-    |> Enum.count()
+    |> Enum.group_by(& &1)
+    |> Enum.map(fn {k, v} -> {k, Enum.count(v)} end)
+    |> Enum.into(%{})
+    |> run(days)
+    |> Map.values()
+    |> Enum.reduce(&Kernel.+/2)
   end
 
-  defp do_simulate(fish, 0, _next_gen), do: fish
-  defp do_simulate([], n, fish), do: do_simulate(fish, n - 1, [])
-  defp do_simulate([0 | rest], n, next_gen), do: do_simulate(rest, n, [6, 8 | next_gen])
-  defp do_simulate([f | rest], n, next_gen), do: do_simulate(rest, n, [f - 1 | next_gen])
+  defp run(world, 0), do: world
+
+  defp run(world, days) do
+    world
+    |> Enum.reduce(%{}, fn
+      {0, v}, h -> h |> Map.put(6, v) |> Map.put(8, v)
+      {k, v}, h -> Map.update(h, k - 1, v, &(&1 + v))
+    end)
+    |> run(days - 1)
+  end
 end
