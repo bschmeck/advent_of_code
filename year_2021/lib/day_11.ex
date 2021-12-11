53e4d8c3238bdc82e312524660e93ee6e0bcaa5a
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-12-11 07:48:11 -0600

    Consolidating parsing steps

--- year_2021/lib/day_11.ex
+++ year_2021/lib/day_11.ex
@@ -1,34 +1,12 @@
 defmodule Day11 do
   def part_one(input, steps) do
-    octopi = 11
-    |> input.contents_of(:stream)
-    |> Stream.map(&String.trim/1)
-    |> Stream.with_index()
-    |> Stream.flat_map(fn {line, y} ->
-      line
-      |> String.split("", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.with_index()
-      |> Enum.map(fn {v, x} -> {{x, y}, v} end)
-    end)
-    |> Enum.into(%{})
+    octopi = parse(input)
 
     step(octopi, steps, 0)
   end
 
   def part_two(input) do
-    octopi = 11
-    |> input.contents_of(:stream)
-    |> Stream.map(&String.trim/1)
-    |> Stream.with_index()
-    |> Stream.flat_map(fn {line, y} ->
-      line
-      |> String.split("", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.with_index()
-      |> Enum.map(fn {v, x} -> {{x, y}, v} end)
-    end)
-    |> Enum.into(%{})
+    octopi = parse(input)
 
     find_synchronized(octopi, Enum.count(octopi), 1)
   end
@@ -95,5 +73,20 @@ defmodule Day11 do
       {x, y + 1},
       {x + 1, y + 1}
     ]
+  end
+
+  defp parse(input) do
+    11
+    |> input.contents_of(:stream)
+    |> Stream.map(&String.trim/1)
+    |> Stream.with_index()
+    |> Stream.flat_map(fn {line, y} ->
+      line
+      |> String.split("", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.with_index()
+      |> Enum.map(fn {v, x} -> {{x, y}, v} end)
+    end)
+    |> Enum.into(%{})
   end
 end
