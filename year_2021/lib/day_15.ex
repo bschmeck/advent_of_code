9ecf6835b43a12559544da013037d3d2e0965a26
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-12-15 13:55:57 -0600

    Day 15, Part 1 fastest

--- year_2021/lib/day_15.ex
+++ year_2021/lib/day_15.ex
@@ -1,5 +1,5 @@
 defmodule Day15 do
-  alias Day15.Route
+  alias Day15.Search
 
   def part_one(input) do
     grid =
@@ -20,68 +20,6 @@ defmodule Day15 do
     goal_y = grid |> Map.keys() |> Enum.map(fn {_x, y} -> y end) |> Enum.max()
     goal = {goal_x, goal_y}
 
-    route = safest(grid, goal, [Route.new(goal)])
-
-    route.total_risk
-  end
-
-  defp safest(_grid, goal, [%Route{current_node: goal} = route | rest]) do
-    # IO.inspect(rest)
-    # nodes = Enum.map(rest, &(&1.current_node))
-    # IO.puts "#{inspect Enum.count(rest)} other routes and #{nodes |> Enum.uniq |> Enum.count} unique locations"
-    # dupes = Enum.frequencies(nodes) |> Enum.filter(fn {k, v} -> v > 1 end) |> Enum.map(fn {k, v} -> k end) |> Enum.take(5) |> Enum.into(MapSet.new())
-    # IO.inspect Enum.filter(rest, fn r -> MapSet.member?(dupes, r.current_node) end)
-    route
-  end
-
-  defp safest(grid, goal, [route | rest]) do
-    # IO.inspect(routes)
-    # IO.puts("#{inspect(route.current_node)} #{route.total_risk} #{Enum.count(rest)}")
-
-    routes =
-      [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
-      |> Enum.map(fn {x_adj, y_adj} ->
-        {elem(route.current_node, 0) + x_adj, elem(route.current_node, 1) + y_adj}
-      end)
-      |> Enum.filter(fn pos -> Map.has_key?(grid, pos) end)
-      |> Enum.reject(fn pos -> Route.visited?(route, pos) end)
-      |> Enum.map(fn pos -> Route.move(route, pos, Map.fetch!(grid, pos), goal) end)
-      |> Enum.sort_by(& &1.estimate)
-      # |> IO.inspect
-      |> insert(rest, [])
-
-    # IO.puts "===================="
-    safest(grid, goal, routes)
-  end
-
-  defp insert([], existing, routes), do: Enum.reverse(routes) ++ existing
-  defp insert(steps, [], routes), do: Enum.reverse(routes) ++ steps
-
-  defp insert(
-         [%Route{current_node: pos} | steps],
-         [%Route{current_node: pos} = existing | rest],
-         routes
-       ) do
-    # We have a cheaper route to pos, drop the new step
-    insert(steps, rest, [existing | routes])
-  end
-
-  defp insert(
-         [%Route{estimate: risk} = step | steps],
-         [%Route{estimate: risk2} | _rest] = existing,
-         routes
-       )
-       when risk <= risk2 do
-    insert(
-      steps,
-      # |> IO.inspect,
-      Enum.reject(existing, fn r -> r.current_node == step.current_node end),
-      [step | routes]
-    )
-  end
-
-  defp insert(steps, [existing | rest], routes) do
-    # IO.puts "Route with #{existing.estimate} is cheaper."
-    insert(steps, rest, [existing | routes])
+    Search.run(grid, goal)
   end
 end
---
+++ year_2021/lib/day_15/search.ex
@@ -0,0 +1,34 @@
+defmodule Day15.Search do
+  defstruct [:goal_x, :goal_y, :seen, :costs]
+
+  def run(grid, {x, y} = _goal) do
+    search = %__MODULE__{goal_x: x, goal_y: y, seen: MapSet.new([{0, 0}]), costs: %{{0, 0} => 0}}
+
+    do_run(grid, search)
+  end
+
+  defp do_run(grid, search) do
+    {{x, y}, risk} = search.costs |> Enum.min_by(fn {_k, v} -> v end)
+
+    if x == search.goal_x && y == search.goal_y do
+      risk
+    else
+      costs = Map.delete(search.costs, {x, y})
+      seen = MapSet.put(search.seen, {x, y})
+
+      costs =
+        [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
+        |> Enum.map(fn {x_adj, y_adj} -> {x + x_adj, y + y_adj} end)
+        |> Enum.filter(fn pos -> Map.has_key?(grid, pos) end)
+        |> Enum.reject(fn pos -> visited?(search, pos) end)
+        |> Enum.map(fn pos -> {pos, risk + Map.fetch!(grid, pos)} end)
+        |> Enum.reduce(costs, fn {pos, new_risk}, costs ->
+          Map.update(costs, pos, new_risk, fn a -> Enum.min([a, new_risk]) end)
+        end)
+
+      do_run(grid, %__MODULE__{search | seen: seen, costs: costs})
+    end
+  end
+
+  defp visited?(%__MODULE__{seen: seen}, pos), do: MapSet.member?(seen, pos)
+end
