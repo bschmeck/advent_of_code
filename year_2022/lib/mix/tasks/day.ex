c4abcdbdb8056e04a43035471b10f1e8680f94e3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-01 14:37:11 -0600

    Simplify the mix task for running a given day

--- year_2022/lib/mix/tasks/day.ex
+++ year_2022/lib/mix/tasks/day.ex
@@ -4,8 +4,27 @@ defmodule Mix.Tasks.Day do
   use Mix.Task
 
   @impl Mix.Task
-  def run(["1.1"]), do: Day01.part_one(InputFile) |> IO.inspect()
-  def run(["1.2"]), do: Day01.part_two(InputFile) |> IO.inspect()
+  def run([arg]) do
+    with [day, part] <- String.split(arg, "."),
+         {:ok, module} <- module_for(day)
+    do
+      case part do
+        "1" -> module.part_one(InputFile) |> IO.inspect()
+        "2" -> module.part_two(InputFile) |> IO.inspect()
+        _ -> Mix.raise("Unsupported part #{part}")
+      end
+    else
+      {:error, msg} -> Mix.raise(msg)
+    _ -> Mix.raise("Unkown failure.")
+    end
+  end
 
-  def run(arg), do: Mix.raise("Day #{arg} has not been implemented.")
+  defp module_for(day) do
+    try do
+      padded = String.pad_leading(day, 2, "0")
+      {:ok, Module.safe_concat(["Day#{padded}"])}
+    rescue
+      ArgumentError -> {:error, "Day #{day} has not been implemented."}
+    end
+  end
 end
