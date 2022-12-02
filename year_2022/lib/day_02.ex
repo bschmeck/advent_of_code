571c55fbf93eabb0ca74ac1dbbcb43f7e2d44349
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-02 09:47:00 -0600

    Day 2, part 2

--- year_2022/lib/day_02.ex
+++ year_2022/lib/day_02.ex
@@ -2,24 +2,50 @@ defmodule Day02 do
   def part_one(input \\ InputFile) do
     input.contents_of(2, :stream)
     |> Enum.map(&String.trim_trailing(&1))
-    |> Enum.map(fn line -> String.split(line, " ") end)
+    |> Enum.map(fn line -> String.split(line, " ") |> Enum.map(&translate/1) end)
     |> Enum.map(&score(&1))
     |> Enum.reduce(0, &Kernel.+/2)
   end
 
+  def part_two(input \\ InputFile) do
+    input.contents_of(2, :stream)
+    |> Enum.map(&String.trim_trailing(&1))
+    |> Enum.map(fn line -> String.split(line, " ") |> choose() end)
+    |> Enum.map(&score(&1))
+    |> Enum.reduce(0, &Kernel.+/2)
+  end
+
+  defp choose([them, "X"]), do: loser(them)
+  defp choose([them, "Y"]), do: draw(them)
+  defp choose([them, "Z"]), do: winner(them)
+
+  defp loser("A"), do: [:rock, :scissors]
+  defp loser("B"), do: [:paper, :rock]
+  defp loser("C"), do: [:scissors, :paper]
+  defp draw(them), do: [translate(them), translate(them)]
+  defp winner("A"), do: [:rock, :paper]
+  defp winner("B"), do: [:paper, :scissors]
+  defp winner("C"), do: [:scissors, :rock]
+
+
+  defp translate("A"), do: :rock
+  defp translate("B"), do: :paper
+  defp translate("C"), do: :scissors
+  defp translate("X"), do: :rock
+  defp translate("Y"), do: :paper
+  defp translate("Z"), do: :scissors
+
   defp score([them, us]), do: outcome(them, us) + shape_score(us)
 
-  defp shape_score("X"), do: 1
-  defp shape_score("Y"), do: 2
-  defp shape_score("Z"), do: 3
+  defp shape_score(:rock), do: 1
+  defp shape_score(:paper), do: 2
+  defp shape_score(:scissors), do: 3
 
-  defp outcome("A", "X"), do: 3
-  defp outcome("A", "Y"), do: 6
-  defp outcome("A", "Z"), do: 0
-  defp outcome("B", "X"), do: 0
-  defp outcome("B", "Y"), do: 3
-  defp outcome("B", "Z"), do: 6
-  defp outcome("C", "X"), do: 6
-  defp outcome("C", "Y"), do: 0
-  defp outcome("C", "Z"), do: 3
+  defp outcome(shape, shape), do: 3
+  defp outcome(:rock, :paper), do: 6
+  defp outcome(:rock, :scissors), do: 0
+  defp outcome(:paper, :rock), do: 0
+  defp outcome(:paper, :scissors), do: 6
+  defp outcome(:scissors, :rock), do: 6
+  defp outcome(:scissors, :paper), do: 0
 end
--- year_2022/test/day_02_test.exs
+++ year_2022/test/day_02_test.exs
@@ -1,7 +1,11 @@
 defmodule Day02Test do
   use ExUnit.Case, async: true
 
-  test "it sums the total score" do
+  test "part_one sums the total score" do
     assert Day02.part_one(InputTestFile) == 15
+  end
+
+  test "part_two chooses each shape and sums the total score" do
+    assert Day02.part_two(InputTestFile) == 12
   end
 end
