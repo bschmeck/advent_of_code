410738347c0e31fa1c7fda5a304da277264e31d5
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-06 09:36:29 -0600

    Day 6, part two

--- year_2022/lib/day_06.ex
+++ year_2022/lib/day_06.ex
@@ -1,14 +1,21 @@
 defmodule Day06 do
   def part_one(input \\ InputFile) do
-    input.contents_of(6) |> marker_location()
+    input.contents_of(6) |> packet_location()
   end
 
-  def marker_location(str) do
+  def part_two(input \\ InputFile) do
+    input.contents_of(6) |> message_location()
+  end
+
+  def packet_location(str), do: locate(str, 4)
+  def message_location(str), do: locate(str, 14)
+
+  defp locate(str, size) do
     str
     |> String.split("", trim: true)
-    |> Enum.chunk_every(4, 1, :discard)
-    |> Enum.take_while(fn substr -> substr |> Enum.uniq() |> Enum.count() != 4 end)
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.take_while(fn substr -> substr |> Enum.uniq() |> Enum.count() != size end)
     |> Enum.count
-    |> Kernel.+(4)
+    |> Kernel.+(size)
   end
 end
--- year_2022/test/day_06_test.exs
+++ year_2022/test/day_06_test.exs
@@ -2,10 +2,18 @@ defmodule Day06Test do
   use ExUnit.Case, async: true
 
   test "it can find the start of packet location" do
-    assert Day06.marker_location("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
-    assert Day06.marker_location("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-    assert Day06.marker_location("nppdvjthqldpwncqszvftbrmjlhg") == 6
-    assert Day06.marker_location("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
-    assert Day06.marker_location("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+    assert Day06.packet_location("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+    assert Day06.packet_location("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+    assert Day06.packet_location("nppdvjthqldpwncqszvftbrmjlhg") == 6
+    assert Day06.packet_location("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+    assert Day06.packet_location("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+  end
+
+  test "it can find the start of message location" do
+    assert Day06.message_location("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19
+    assert Day06.message_location("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
+    assert Day06.message_location("nppdvjthqldpwncqszvftbrmjlhg") == 23
+    assert Day06.message_location("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29
+    assert Day06.message_location("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26
   end
 end
