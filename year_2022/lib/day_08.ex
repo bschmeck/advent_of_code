a99b596712c2a76ce7344700a8a32e5e1fd957a6
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-08 10:21:28 -0600

    Use the Grid module's helper functions to simplify looking at trees

--- year_2022/lib/day_08.ex
+++ year_2022/lib/day_08.ex
@@ -18,8 +18,6 @@ defmodule Day08 do
     |> Enum.max()
   end
 
-  defp visible?({x, _}, %Day08.Grid{width: w}) when x == 0 or x + 1 == w, do: true
-  defp visible?({_, y}, %Day08.Grid{height: h}) when y == 0 or y + 1 == h, do: true
   defp visible?(point, grid) do
     tree = Map.get(grid.map, point)
 
@@ -30,10 +28,10 @@ defmodule Day08 do
     end)
   end
 
-  defp points_to({x, y}, _grid, :left), do: for x2 <- 0..x-1, do: {x2, y}
-  defp points_to({x, y}, %Day08.Grid{width: w}, :right), do: for x2 <- w-1..x+1, do: {x2, y}
-  defp points_to({x, y}, _grid, :up), do: for y2 <- 0..y-1, do: {x, y2}
-  defp points_to({x, y}, %Day08.Grid{height: h}, :down), do: for y2 <- h-1..y+1, do: {x, y2}
+  defp points_to({x, y}, grid, :left), do: grid |> Day08.Grid.row(y) |> Enum.take_while(fn {x2, _} -> x2 < x end)
+  defp points_to({x, y}, grid, :right), do: grid |> Day08.Grid.row(y) |> Enum.reverse() |> Enum.take_while(fn {x2, _} -> x2 > x end)
+  defp points_to({x, y}, grid, :up), do: grid |> Day08.Grid.col(x) |> Enum.take_while(fn {_, y2} -> y2 < y end)
+  defp points_to({x, y}, grid, :down), do: grid |> Day08.Grid.col(x) |> Enum.reverse() |> Enum.take_while(fn {_, y2} -> y2 > y end)
 
   defp scenic_score(point, grid) do
     tree = Map.get(grid.map, point)
@@ -47,14 +45,7 @@ defmodule Day08 do
     |> Enum.reduce(&Kernel.*/2)
   end
 
-  defp points_from({0, _y}, _grid, :left), do: []
-  defp points_from({x, y}, _grid, :left), do: for x2 <- x-1..0, do: {x2, y}
-  defp points_from({x, _y}, %Day08.Grid{width: w}, :right) when x == w - 1, do: []
-  defp points_from({x, y}, %Day08.Grid{width: w}, :right), do: for x2 <- x+1..w-1, do: {x2, y}
-  defp points_from({_x, 0}, _grid, :up), do: []
-  defp points_from({x, y}, _grid, :up), do: for y2 <- y-1..0, do: {x, y2}
-  defp points_from({_x, y}, %Day08.Grid{height: h}, :down) when y == h - 1, do: []
-  defp points_from({x, y}, %Day08.Grid{height: h}, :down), do: for y2 <- y+1..h-1, do: {x, y2}
+  defp points_from(p, grid, dir), do: points_to(p, grid, dir) |> Enum.reverse()
 
   defp count_view([], _tree, count), do: count
   defp count_view([height | _], tree, count) when height >= tree, do: count + 1
