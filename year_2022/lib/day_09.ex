8e19278125431bf14bf1e3295dcfc66af8c42379
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-09 09:33:03 -0600

    Day 9, parts one and two

--- year_2022/lib/day_09.ex
+++ year_2022/lib/day_09.ex
@@ -1,9 +1,43 @@
 defmodule Day09 do
   def part_one(input \\ InputFile) do
-
+    simulate(input, 2)
   end
 
   def part_two(input \\ InputFile) do
-
+    simulate(input, 10)
   end
+
+  defp simulate(input, number_of_knots) do
+    knots = for _ <- 1..number_of_knots, do: {0, 0}
+
+    input.contents_of(9, :stream)
+    |> Enum.map(fn l -> String.split(l, " ") end)
+    |> Enum.map(fn [dir, amt] -> {dir, String.to_integer(amt)} end)
+    |> move(knots, MapSet.new())
+    |> MapSet.size()
+  end
+
+  defp move([], _knots, points), do: points
+  defp move([{_dir, 0} | rest], knots, points), do: move(rest, knots, points)
+  defp move(moves = [{"R", _amt} | _rest], [{x, y} | rope], points), do: do_move(moves, [{x + 1, y} | rope], points)
+  defp move(moves = [{"L", _amt} | _rest], [{x, y} | rope], points), do: do_move(moves, [{x - 1, y} | rope], points)
+  defp move(moves = [{"U", _amt} | _rest], [{x, y} | rope], points), do: do_move(moves, [{x, y + 1} | rope], points)
+  defp move(moves = [{"D", _amt} | _rest], [{x, y} | rope], points), do: do_move(moves, [{x, y - 1} | rope], points)
+
+  defp do_move([{dir, amt} | rest], rope, points) do
+    {rope, tail} = adjust(rope)
+    move([{dir, amt - 1} | rest], rope, MapSet.put(points, tail))
+  end
+
+  defp adjust(rope), do: adjust(rope, [])
+  defp adjust([tail], adjusted), do: {Enum.reverse([tail | adjusted]), tail}
+  defp adjust([h, t | rest], adjusted) do
+    t = adjust_knots(h, t)
+    adjust([t | rest], [h | adjusted])
+  end
+
+  def adjust_knots({h_x, h_y}, {t_x, t_y}) when abs(h_x - t_x) <= 1 and abs(h_y - t_y) <= 1, do: {t_x, t_y}
+  def adjust_knots({h_x, h_y}, {t_x, t_y}) when abs(h_x - t_x) == 1, do: {h_x, div(h_y + t_y, 2)}
+  def adjust_knots({h_x, h_y}, {t_x, t_y}) when abs(h_y - t_y) == 1, do: {div(h_x + t_x, 2), h_y}
+  def adjust_knots({h_x, h_y}, {t_x, t_y}), do: {div(h_x + t_x, 2), div(h_y + t_y, 2)}
 end
--- year_2022/test/day_09_test.exs
+++ year_2022/test/day_09_test.exs
@@ -1,13 +1,60 @@
 defmodule Day09Test do
   use ExUnit.Case, async: true
 
-  @tag :skip
   test "it can solve part one" do
-
+    assert Day09.part_one(InputTestFile) == 13
   end
 
-  @tag :skip
   test "it can solve part two" do
+    assert Day09.part_two(InputTestFile) == 1
+  end
 
+  test "it doesn't change points that are on top of each other" do
+    assert Day09.adjust_knots({3, 4}, {3, 4}) == {3, 4}
+  end
+
+  test "it doesn't change when the tail is to the left" do
+    assert Day09.adjust_knots({3, 4}, {2, 4}) == {2, 4}
+  end
+
+  test "it doesn't change when the tail is to the right" do
+    assert Day09.adjust_knots({3, 4}, {4, 4}) == {4, 4}
+  end
+
+  test "it doesn't change when the tail is above" do
+    assert Day09.adjust_knots({3, 4}, {3, 3}) == {3, 3}
+  end
+
+  test "it doesn't change when the tail is below" do
+    assert Day09.adjust_knots({3, 4}, {3, 5}) == {3, 5}
+  end
+
+  test "it doesn't change when the tail is diagonally adjacent" do
+    assert Day09.adjust_knots({3, 4}, {2, 3}) == {2, 3}
+  end
+
+  test "it closes the gap when the tail is too far left" do
+    assert Day09.adjust_knots({3, 4}, {1, 4}) == {2, 4}
+  end
+
+  test "it closes the gap when the tail is too far rightt" do
+    assert Day09.adjust_knots({3, 4}, {5, 4}) == {4, 4}
+  end
+
+  test "it closes the gap when the tail is too far above" do
+    assert Day09.adjust_knots({3, 4}, {3, 2}) == {3, 3}
+  end
+
+  test "it closes the gap when the tail is too far below" do
+    assert Day09.adjust_knots({3, 4}, {3, 6}) == {3, 5}
+  end
+
+  test "it closes the gap when the tail is too far away diagonally" do
+    assert Day09.adjust_knots({3, 4}, {2, 6}) == {3, 5}
+    assert Day09.adjust_knots({3, 4}, {4, 6}) == {3, 5}
+    assert Day09.adjust_knots({3, 4}, {2, 2}) == {3, 3}
+    assert Day09.adjust_knots({3, 4}, {4, 2}) == {3, 3}
+    assert Day09.adjust_knots({2, 2}, {0, 0}) == {1, 1}
+    assert Day09.adjust_knots({-2, 2}, {0, 0}) == {-1, 1}
   end
 end
