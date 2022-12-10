1b1670499469a7812b42e662319fca63fdd1a43d
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-10 07:55:59 -0600

    Fetch Day 10

---
+++ year_2022/lib/day_10.ex
@@ -0,0 +1,9 @@
+defmodule Day10 do
+  def part_one(input \\ InputFile) do
+
+  end
+
+  def part_two(input \\ InputFile) do
+
+  end
+end
---
+++ year_2022/test/day_10_test.exs
@@ -0,0 +1,13 @@
+defmodule Day10Test do
+  use ExUnit.Case, async: true
+
+  @tag :skip
+  test "it can solve part one" do
+    # Day10.part_one(InputTestFile) == nil
+  end
+
+  @tag :skip
+  test "it can solve part two" do
+    # Day10.part_two(InputTestFile) == nil
+  end
+end
