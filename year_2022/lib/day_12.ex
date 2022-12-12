6769ab30b524e6cf100246e37ca2a0bfc5f1bcb2
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-12 12:45:40 -0600

    Day 12, part two

--- year_2022/lib/day_12.ex
+++ year_2022/lib/day_12.ex
@@ -8,34 +8,55 @@ defmodule Day12 do
     ascend(grid, [{83, start, 0}], [], MapSet.new([start]))
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    grid = input.contents_of(12, :stream)
+    |> Grid.build(fn line -> String.to_charlist(line) end)
 
+    start = Enum.find(grid.map, fn {_point, char} -> char == 69 end) |> elem(0)
+
+    descend(grid, [{69, start, 0}], [], MapSet.new([start]))
   end
 
   defp ascend(_, [], [], _), do: -1
   defp ascend(grid, [], routes, visited), do: ascend(grid, routes, [], visited)
   defp ascend(_grid, [{69, _point, count} | _rest], _next, _visited), do: count
   defp ascend(grid, [loc = {_, _, count} | rest], next, visited) do
-    points = steps(grid, loc, visited)
+    points = steps(grid, loc, visited, :up)
     visited = Enum.reduce(points, visited, fn p, v -> MapSet.put(v, p) end)
     routes = Enum.map(points, fn p -> {Map.get(grid.map, p), p, count + 1} end)
     ascend(grid, rest, routes ++ next, visited)
   end
 
-  defp steps(grid, {char, {x, y}, _count}, visited) do
+  defp descend(_, [], [], _), do: -1
+  defp descend(grid, [], routes, visited), do: descend(grid, routes, [], visited)
+  defp descend(_grid, [{97, _point, count} | _rest], _next, _visited), do: count
+  defp descend(grid, [loc = {_, _, count} | rest], next, visited) do
+    points = steps(grid, loc, visited, :down)
+    visited = Enum.reduce(points, visited, fn p, v -> MapSet.put(v, p) end)
+    routes = Enum.map(points, fn p -> {Map.get(grid.map, p), p, count + 1} end)
+    descend(grid, rest, routes ++ next, visited)
+  end
+
+  defp steps(grid, {char, {x, y}, _count}, visited, dir) do
     [{-1, 0}, {1, 0}, {0, 1}, {0, -1}]
     |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
     |> Enum.filter(fn point -> on_map?(grid, point) end)
     |> Enum.reject(fn point -> MapSet.member?(visited, point) end)
-    |> Enum.filter(fn point -> valid_step?(char, Map.get(grid.map, point)) end)
+    |> Enum.filter(fn point -> valid_step?(char, Map.get(grid.map, point), dir) end)
   end
 
   defp on_map?(_grid, {x, y}) when x < 0 or y < 0, do: false
   defp on_map?(%Grid{height: h, width: w}, {x, y}) when x >= w or y >= h, do: false
   defp on_map?(_, _), do: true
 
-  defp valid_step?(122, 69), do: true # z -> E
-  defp valid_step?(83, 97), do: true # S -> a
-  defp valid_step?(from, to) when from < 97 or to < 97, do: false
-  defp valid_step?(from, to), do: to <= from + 1
+  def valid_step?(122, 69, :up), do: true # z -> E
+  def valid_step?(83, 97, :up), do: true # S -> a
+  def valid_step?(from, to, :up) when from < 97 or to < 97, do: false
+  def valid_step?(from, to, :up), do: to <= from + 1
+
+  def valid_step?(69, 122, :down), do: true # E -> z
+  def valid_step?(from, to, :down) when from < 97 or to < 97, do: false
+  def valid_step?(from, to, :down) when to >= from, do: true
+  def valid_step?(from, to, :down), do: from == to + 1
+
 end
--- year_2022/test/day_12_test.exs
+++ year_2022/test/day_12_test.exs
@@ -5,8 +5,14 @@ defmodule Day12Test do
     assert Day12.part_one(InputTestFile) == 31
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day12.part_two(InputTestFile) == nil
+    assert Day12.part_two(InputTestFile) == 29
+  end
+
+  test "it can identify valid steps down" do
+    refute Day12.valid_step?(122, 115, :down)
+    refute Day12.valid_step?(122, 117, :down)
+    assert Day12.valid_step?(122, 121, :down)
+    assert Day12.valid_step?(120, 120, :down)
   end
 end
