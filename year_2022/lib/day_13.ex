0d08d40983e2e18f9193439b37852803c2dd22b2
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-13 07:50:35 -0600

    Pair validation rules

--- year_2022/lib/day_13.ex
+++ year_2022/lib/day_13.ex
@@ -6,4 +6,24 @@ defmodule Day13 do
   def part_two(_input \\ InputFile) do
 
   end
+
+  def valid?(left, right) when is_integer(left) and is_integer(right) do
+    case left - right do
+                0 -> :cont
+                i when i < 0 -> true
+                _ -> false
+    end
+  end
+  def valid?([left | _], [right | _]) when is_integer(left) and is_integer(right) and left > right, do: false
+  def valid?([], [_]), do: true
+  def valid?([_], []), do: false
+  def valid?([], []), do: :cont
+  def valid?([left | rest], [right | rest2]) when is_integer(left) and is_list(right), do: valid?([[left] | rest], [right | rest2])
+  def valid?([left | rest], [right | rest2]) when is_list(left) and is_integer(right), do: valid?([left | rest], [[right] | rest2])
+  def valid?(left, right) do
+    case valid?(hd(left), hd(right)) do
+      :cont -> valid?(tl(left), tl(right))
+      res -> res
+    end
+  end
 end
--- year_2022/test/day_13_test.exs
+++ year_2022/test/day_13_test.exs
@@ -1,6 +1,7 @@
 defmodule Day13Test do
   use ExUnit.Case, async: true
 
+  @tag :skip
   test "it can solve part one" do
     assert Day13.part_one(InputTestFile) == 13
   end
@@ -8,5 +9,62 @@ defmodule Day13Test do
   @tag :skip
   test "it can solve part two" do
     assert Day13.part_two(InputTestFile) == nil
+  end
+
+  test "two integers are valid if the left one is smaller" do
+    assert Day13.valid?(1, 3)
+  end
+
+  test "two integers are not valid if the left one is larger" do
+    refute Day13.valid?(7, 3)
+  end
+
+  test "two lists are valid if the left one runs out of elements first" do
+    assert Day13.valid?([], [3])
+  end
+
+  test "two lists are not valid if the right one runs out of elements first" do
+    refute Day13.valid?([13], [])
+  end
+
+  test "it converts an integer to a list if needed" do
+    assert Day13.valid?([1], [[3]])
+    refute Day13.valid?([7], [[3]])
+    assert Day13.valid?([[1]], [3])
+    refute Day13.valid?([[7]], [3])
+  end
+
+  test "it compares the lists from the example correctly" do
+    left = [1, 1, 3, 1, 1]
+    right = [1, 1, 5, 1, 1]
+    assert Day13.valid?(left, right)
+
+    left = [[1], [2, 3, 4]]
+    right = [[1], 4]
+    assert Day13.valid?(left, right)
+
+    left = [9]
+    right = [[8,7,6]]
+    refute Day13.valid?(left, right)
+
+    left = [[4,4],4,4]
+    right = [[4,4],4,4,4]
+    assert Day13.valid?(left, right)
+
+    left = [7,7,7,7]
+    right = [7,7,7]
+    refute Day13.valid?(left, right)
+
+    left = []
+    right = [3]
+    assert Day13.valid?(left, right)
+
+    left = [[[]]]
+    right = [[]]
+    refute Day13.valid?(left, right)
+
+    left = [1,[2,[3,[4,[5,6,7]]]],8,9]
+    right = [1,[2,[3,[4,[5,6,0]]]],8,9]
+    refute Day13.valid?(left, right)
   end
 end
