924ec7d7c8d2bf65a2be3659b246a6d3acb06d4f
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-15 11:10:46 -0600

    Day 14, parts one and two

--- year_2022/lib/day_14.ex
+++ year_2022/lib/day_14.ex
@@ -1,9 +1,70 @@
 defmodule Day14 do
   def part_one(input \\ InputFile) do
+    cave = input.contents_of(14, :stream)
+    |> Enum.map(&path/1)
+    |> Enum.reduce(&MapSet.union/2)
 
+    depth = MapSet.to_list(cave) |> Enum.map(fn {_x, y} -> y end) |> Enum.max()
+
+    fill(cave, depth, 0)
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    cave = input.contents_of(14, :stream)
+    |> Enum.map(&path/1)
+    |> Enum.reduce(&MapSet.union/2)
 
+    depth = MapSet.to_list(cave) |> Enum.map(fn {_x, y} -> y end) |> Enum.max() |> Kernel.+(2)
+
+    fill_2(cave, depth, 0)
+  end
+
+  def path(str) do
+    str
+    |> String.split(" -> ")
+    |> Enum.map(fn pair -> pair |> String.split(",") |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn [x, y] -> {x, y} end)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.flat_map(fn [from, to] -> line(from, to) end)
+    |> MapSet.new()
+  end
+
+  defp line({from, y}, {to, y}), do: for x <- from..to, do: {x, y}
+  defp line({x, from}, {x, to}), do: for y <- from..to, do: {x, y}
+
+  defp fill(cave, depth, count) do
+    case settle(cave, {500, 0}, depth) do
+      :inf -> count
+      point -> fill(MapSet.put(cave, point), depth, count + 1)
+    end
+  end
+
+  defp settle(_cave, {_x, y}, depth) when y > depth, do: :inf
+  defp settle(cave, {x, y}, depth) do
+    [{0, 1}, {-1, 1}, {1, 1}]
+    |> Enum.map(fn {x_d, y_d} -> {x + x_d, y + y_d} end)
+    |> Enum.find(fn next -> !MapSet.member?(cave, next) end)
+    |> case do
+        nil -> {x, y}
+        next -> settle(cave, next, depth)
+    end
+  end
+
+  defp fill_2(cave, depth, count) do
+    case settle_2(cave, {500, 0}, depth) do
+      {500, 0} -> count + 1
+      point -> fill_2(MapSet.put(cave, point), depth, count + 1)
+    end
+  end
+
+  defp settle_2(_cave, {x, y}, depth) when y + 1 == depth, do: {x, y}
+  defp settle_2(cave, {x, y}, depth) do
+    [{0, 1}, {-1, 1}, {1, 1}]
+    |> Enum.map(fn {x_d, y_d} -> {x + x_d, y + y_d} end)
+    |> Enum.find(fn next -> !MapSet.member?(cave, next) end)
+    |> case do
+        nil -> {x, y}
+        next -> settle_2(cave, next, depth)
+    end
   end
 end
--- year_2022/test/day_14_test.exs
+++ year_2022/test/day_14_test.exs
@@ -5,8 +5,11 @@ defmodule Day14Test do
     assert Day14.part_one(InputTestFile) == 24
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day14.part_two(InputTestFile) == nil
+    assert Day14.part_two(InputTestFile) == 93
+  end
+
+  test "it converts a path to a set of points" do
+    assert Day14.path("498,4 -> 498,6 -> 496,6") == MapSet.new([{498, 4}, {498, 5}, {498, 6}, {497, 6}, {496, 6}])
   end
 end
