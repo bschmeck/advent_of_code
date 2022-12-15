2fbdb6bcbbececa6b9a620c7cd9fca092691a648
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-15 10:02:41 -0600

    Day 15, part one

--- year_2022/lib/day_15.ex
+++ year_2022/lib/day_15.ex
@@ -1,9 +1,42 @@
 defmodule Day15 do
-  def part_one(input \\ InputFile) do
+  def part_one(input \\ InputFile, target) do
+    data = input.contents_of(15, :stream)
+    |> Enum.map(&parse/1)
 
+    beacons = data |> Enum.map(&elem(&1, 1)) |> Enum.filter(fn {_x, y} -> y == target end) |> MapSet.new()
+
+    data
+    |> Enum.map(fn {sensor, beacon} -> coverage(sensor, beacon, target) end)
+    |> Enum.reduce(&MapSet.union/2)
+    |> MapSet.difference(beacons)
+    |> MapSet.size()
   end
 
   def part_two(_input \\ InputFile) do
 
   end
+
+  defp parse(line) do
+    regex = ~r/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/
+
+    [_, s_x, s_y, b_x, b_y] = Regex.run(regex, line)
+
+    {
+      {String.to_integer(s_x), String.to_integer(s_y)},
+      {String.to_integer(b_x), String.to_integer(b_y)},
+    }
+  end
+
+  def coverage({x, y} = sensor, beacon, target) do
+    max_dist = distance(sensor, beacon)
+    case abs(y - target) do
+      y_dist when y_dist <= max_dist ->
+        (for delta <- 0..(max_dist - y_dist), do: [{x + delta, target}, {x - delta, target}])
+        |> Enum.concat
+        |> MapSet.new()
+      _ -> MapSet.new()
+    end
+  end
+
+  defp distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
 end
--- year_2022/test/day_15_test.exs
+++ year_2022/test/day_15_test.exs
@@ -2,11 +2,19 @@ defmodule Day15Test do
   use ExUnit.Case, async: true
 
   test "it can solve part one" do
-    assert Day15.part_one(InputTestFile) == 26
+    assert Day15.part_one(InputTestFile, 10) == 26
   end
 
   @tag :skip
   test "it can solve part two" do
     assert Day15.part_two(InputTestFile) == nil
+  end
+
+  test "it returns no points when the target is too far away" do
+    assert Day15.coverage({3, 4}, {4, 5}, 10) == MapSet.new()
+  end
+
+  test "it returns reachable points at the target line" do
+    assert Day15.coverage({3, 4}, {3, 12}, 10) == MapSet.new([{3, 10}, {2, 10}, {4, 10}, {1, 10}, {5, 10}])
   end
 end
