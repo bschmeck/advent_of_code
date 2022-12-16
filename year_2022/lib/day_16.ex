d941ee02ed0b2f8a8384566bb5898cbc0d4bea25
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-16 09:24:40 -0600

    Graph building

--- year_2022/lib/day_16.ex
+++ year_2022/lib/day_16.ex
@@ -1,9 +1,17 @@
 defmodule Day16 do
   def part_one(input \\ InputFile) do
-
+    input.contents_of(16, :stream)
+    |> Enum.map(&parse_valve/1)
+    |> Map.new()
   end
 
   def part_two(_input \\ InputFile) do
 
+  end
+
+  defp parse_valve(line) do
+    [_, name, rate, tunnels] = Regex.run(~r/^Valve (.*) has flow rate=(.*); tunnels? leads? to valves? (.*)$/, line)
+
+    {name, {String.to_integer(rate), String.split(tunnels, ", ")}}
   end
 end
