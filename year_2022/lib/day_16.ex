129a559b55e60bc100cd94f3a470d53fb692edfc
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-16 10:25:56 -0600

    Day 16 flailing around

--- year_2022/lib/day_16.ex
+++ year_2022/lib/day_16.ex
@@ -1,8 +1,13 @@
 defmodule Day16 do
   def part_one(input \\ InputFile) do
-    input.contents_of(16, :stream)
+    valves = input.contents_of(16, :stream)
     |> Enum.map(&parse_valve/1)
     |> Map.new()
+
+    targets = valves
+    |> Enum.filter(fn {name, {rate, _}} -> rate > 0 end)
+    |> Enum.map(&elem(&1, 0))
+    |> MapSet.new()
   end
 
   def part_two(_input \\ InputFile) do
@@ -14,4 +19,6 @@ defmodule Day16 do
 
     {name, {String.to_integer(rate), String.split(tunnels, ", ")}}
   end
+
+  def total_pressure_released(flow_rate, minutes_remaining), do: flow_rate * minutes_remaining
 end
--- year_2022/test/day_16_test.exs
+++ year_2022/test/day_16_test.exs
@@ -9,4 +9,16 @@ defmodule Day16Test do
   test "it can solve part two" do
     assert Day16.part_two(InputTestFile) == nil
   end
+
+  test "it can compute how much pressure will be released by a valve" do
+    assert Day16.total_pressure_released(13, 28) == 364
+  end
+
+  test "it can sum pressure released by valves over time" do
+    total = [{20, 2}, {13, 5}, {21, 9}, {22, 17}, {3, 21}, {2, 24}]
+    |> Enum.map(fn {rate, opened_at} ->  Day16.total_pressure_released(rate, 30 - opened_at) end)
+    |> Enum.sum()
+
+    assert total == 1651
+  end
 end
