2d33d4d43fbbc589575d7d6ffac6db7e923b1408
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2022-12-19 08:18:59 -0600

    Fetch days 18 and 19

---
+++ year_2022/test/day_19_test.exs
@@ -0,0 +1,12 @@
+defmodule Day19Test do
+  use ExUnit.Case, async: true
+
+  test "it can solve part one" do
+    assert Day19.part_one(InputTestFile) == nil
+  end
+
+  @tag :skip
+  test "it can solve part two" do
+    assert Day19.part_two(InputTestFile) == nil
+  end
+end
