a3bbc1208e80ff3db3fbd84445adb230ca705440
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2023-12-01 14:34:24 -0600

    Day 01, Part 2

--- year_2023/lib/day_01.ex
+++ year_2023/lib/day_01.ex
@@ -3,7 +3,15 @@ defmodule Day01 do
     input.contents_of(1, :stream)
     |> Enum.map(fn line -> extract_digits(line) end)
     |> Enum.map(fn digits -> [hd(digits), hd(Enum.reverse(digits))] end)
-    |> Enum.map(fn [tens, ones] -> String.to_integer(tens) * 10 + String.to_integer(ones) end)
+    |> Enum.map(fn [tens, ones] -> tens * 10 + ones end)
+    |> Enum.sum
+  end
+
+  def part_two(input \\ InputFile) do
+    input.contents_of(1, :stream)
+    |> Enum.map(fn line -> extract_written_digits(line) end)
+    |> Enum.map(fn digits -> [hd(digits), hd(Enum.reverse(digits))] end)
+    |> Enum.map(fn [tens, ones] -> tens * 10 + ones end)
     |> Enum.sum
   end
 
@@ -11,5 +19,28 @@ defmodule Day01 do
     line
     |> String.split("", trim: true)
     |> Enum.filter(&Regex.match?(~r/[[:digit:]]/, &1))
+    |> Enum.map(&String.to_integer/1)
   end
+
+  def extract_written_digits(line), do: extract_written_digits(line, [])
+  defp extract_written_digits("", digits), do: Enum.reverse(digits)
+  defp extract_written_digits("1" <> rest, digits), do: extract_written_digits(rest, [1 | digits])
+  defp extract_written_digits("2" <> rest, digits), do: extract_written_digits(rest, [2 | digits])
+  defp extract_written_digits("3" <> rest, digits), do: extract_written_digits(rest, [3 | digits])
+  defp extract_written_digits("4" <> rest, digits), do: extract_written_digits(rest, [4 | digits])
+  defp extract_written_digits("5" <> rest, digits), do: extract_written_digits(rest, [5 | digits])
+  defp extract_written_digits("6" <> rest, digits), do: extract_written_digits(rest, [6 | digits])
+  defp extract_written_digits("7" <> rest, digits), do: extract_written_digits(rest, [7 | digits])
+  defp extract_written_digits("8" <> rest, digits), do: extract_written_digits(rest, [8 | digits])
+  defp extract_written_digits("9" <> rest, digits), do: extract_written_digits(rest, [9 | digits])
+  defp extract_written_digits("one" <> rest, digits), do: extract_written_digits("e" <> rest, [1 | digits])
+  defp extract_written_digits("two" <> rest, digits), do: extract_written_digits("o" <> rest, [2 | digits])
+  defp extract_written_digits("three" <> rest, digits), do: extract_written_digits("e" <> rest, [3 | digits])
+  defp extract_written_digits("four" <> rest, digits), do: extract_written_digits(rest, [4 | digits])
+  defp extract_written_digits("five" <> rest, digits), do: extract_written_digits("e" <> rest, [5 | digits])
+  defp extract_written_digits("six" <> rest, digits), do: extract_written_digits(rest, [6 | digits])
+  defp extract_written_digits("seven" <> rest, digits), do: extract_written_digits("n" <> rest, [7 | digits])
+  defp extract_written_digits("eight" <> rest, digits), do: extract_written_digits("t" <> rest, [8 | digits])
+  defp extract_written_digits("nine" <> rest, digits), do: extract_written_digits("e" <> rest, [9 | digits])
+  defp extract_written_digits(<<_::binary-size(1)>> <> rest, digits), do: extract_written_digits(rest, digits)
 end
--- year_2023/test/day_01_test.exs
+++ year_2023/test/day_01_test.exs
@@ -6,6 +6,12 @@ defmodule Day01Test do
   end
 
   test "it finds the digits in a string" do
-    assert Day01.extract_digits("ab1cd2ef3") == ["1", "2", "3"]
+    assert Day01.extract_digits("ab1cd2ef3") == [1, 2, 3]
+  end
+
+  test "it finds digits written as words" do
+    assert Day01.extract_written_digits("two1nine") == [2, 1, 9]
+    assert Day01.extract_written_digits("abcone2threexyz") == [1, 2, 3]
+    assert Day01.extract_written_digits("xtwone3four") == [2, 3, 4]
   end
 end
