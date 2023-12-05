3b28a5fe4449c2d87963ef662a6b4025e618b84b
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2023-12-05 17:04:11 -0600

    Day 05, Part 2

--- year_2023/lib/day_05.ex
+++ year_2023/lib/day_05.ex
@@ -1,31 +1,50 @@
 defmodule Day05 do
   defmodule Mapping do
-    defstruct [:func, :range]
+    defstruct [:func, :range, :min, :max]
 
     def build(dst, src, len) do
-      %__MODULE__{func: fn val -> dst + val - src end, range: src..(src + len - 1)}
+      %__MODULE__{func: fn val -> dst + val - src end, range: src..(src + len - 1), min: src, max: src + len - 1}
     end
   end
 
   def part_one(input \\ InputFile) do
     [seeds | raw_mappers] = input.contents_of(5) |> String.trim_trailing() |> String.split("\n\n")
 
-    seeds = seeds |> String.replace_prefix("seeds: ", "") |> String.split(" ") |> Enum.map(&String.to_integer/1)
+    seeds = seeds
+    |> String.replace_prefix("seeds: ", "")
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&({&1, &1}))
 
     raw_mappers
     |> Enum.map(&build_mappings/1)
-    |> Enum.reduce(seeds, fn mapper, seeds -> Enum.map(seeds, fn seed -> map(seed, mapper) end) end)
+    |> Enum.reduce(seeds, fn mappings, seeds -> map(mappings, seeds) end)
+    |> Enum.map(fn {min, _max} -> min end)
     |> Enum.min()
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    [seeds | raw_mappers] = input.contents_of(5) |> String.trim_trailing() |> String.split("\n\n")
 
+    seeds = seeds
+    |> String.replace_prefix("seeds: ", "")
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [min, size] -> {min, min + size - 1} end)
+
+    raw_mappers
+    |> Enum.map(&build_mappings/1)
+    |> Enum.reduce(seeds, fn mappings, seeds -> map(mappings, seeds) end)
+    |> Enum.map(fn {min, _max} -> min end)
+    |> Enum.min()
   end
 
-  def map(val, mappers) do
-    mapping = Enum.find(mappers, Mapping.build(val, val, 1), fn mapper -> val in mapper.range end)
-
-    mapping.func.(val)
+  def map(mappings, seeds), do: map(mappings, [], seeds)
+  defp map([], mapped, seeds), do: mapped ++ seeds
+  defp map([mapping | mappers], mapped, seeds) do
+    {new_mapped, seeds} = apply_mapping(mapping, seeds, [], [])
+    map(mappers, new_mapped ++ mapped, seeds)
   end
 
   def build_mappings(raw) do
@@ -35,5 +54,24 @@ defmodule Day05 do
     |> Enum.map(fn line -> String.split(line, " ") end)
     |> Enum.map(fn pieces -> Enum.map(pieces, &String.to_integer/1) end)
     |> Enum.map(fn [dst, src, len] -> Mapping.build(dst, src, len) end)
+  end
+
+  def apply_mapping(_mapping, [], mapped, unmapped), do: {mapped, unmapped}
+  def apply_mapping(mapping, [seed | rest], mapped, unmapped) do
+    case transform(seed, mapping) do
+      nil -> apply_mapping(mapping, rest, mapped, [seed | unmapped])
+      {new_mapped, seeds} -> apply_mapping(mapping, rest, [new_mapped | mapped], seeds ++ unmapped)
+    end
+  end
+
+  def transform({min, max}, mapping) do
+    cond do
+      max < mapping.min -> nil
+      min > mapping.max -> nil
+      min in mapping.range and max in mapping.range -> {{mapping.func.(min), mapping.func.(max)}, []}
+      min in mapping.range and max not in mapping.range -> {{mapping.func.(min), mapping.func.(mapping.max)}, [{mapping.max + 1, max}]}
+      min not in mapping.range and max in mapping.range -> {{mapping.func.(mapping.min), mapping.func.(max)}, [{min, mapping.min - 1}]}
+      min < mapping.min and max > mapping.max -> {{mapping.func.(mapping.min), mapping.func.(mapping.max)}, [{min, mapping.min - 1}, {mapping.max + 1, max}]}
+    end
   end
 end
--- year_2023/test/day_05_test.exs
+++ year_2023/test/day_05_test.exs
@@ -5,9 +5,8 @@ defmodule Day05Test do
     assert Day05.part_one(InputTestFile) == 35
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day05.part_two(InputTestFile) == nil
+    assert Day05.part_two(InputTestFile) == 46
   end
 
   test "it can build a mapping function" do
@@ -19,12 +18,36 @@ defmodule Day05Test do
   test "it can chain multiple mappers" do
     mappings = [Day05.Mapping.build(50, 98, 2), Day05.Mapping.build(52, 50, 48)]
 
-    assert Day05.map(0, mappings) == 0
-    assert Day05.map(1, mappings) == 1
-    assert Day05.map(49, mappings) == 49
-    assert Day05.map(50, mappings) == 52
-    assert Day05.map(51, mappings) == 53
-    assert Day05.map(98, mappings) == 50
-    assert Day05.map(99, mappings) == 51
+    assert Day05.map(mappings, [{0, 0}]) == [{0, 0}]
+    assert Day05.map(mappings, [{1, 1}]) == [{1, 1}]
+    assert Day05.map(mappings, [{49, 49}]) == [{49, 49}]
+    assert Day05.map(mappings, [{50, 50}]) == [{52, 52}]
+    assert Day05.map(mappings, [{51, 51}]) == [{53, 53}]
+    assert Day05.map(mappings, [{98, 98}]) == [{50, 50}]
+    assert Day05.map(mappings, [{99, 99}]) == [{51, 51}]
+  end
+
+  test "it doesn't transform a range when the mapping is less than the range min" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 43, 6)) == nil
+  end
+
+  test "it can transform a range when the mapping is greater than the range max" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 90, 6)) == nil
+  end
+
+  test "it can transform a range when the range is contained by the mapping" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 50, 20)) == {{19, 28}, []}
+  end
+
+  test "it can transform a range when the mapping overlaps on the lower end" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 50, 10)) == {{19, 21}, [{60, 66}]}
+  end
+
+  test "it can transform a range when the mapping overlaps on the higher end" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 60, 10)) == {{12, 18}, [{57, 59}]}
+  end
+
+  test "it can transform a range when the range fully contains the mapping" do
+    assert Day05.transform({57, 66}, Day05.Mapping.build(12, 60, 4)) == {{12, 15}, [{57, 59}, {64, 66}]}
   end
 end
