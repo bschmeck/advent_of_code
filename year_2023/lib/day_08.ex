0b33dfb28655af148bc56c6c36c142884959b3d2
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2023-12-08 14:27:29 -0600

    Day 08, Part 2

--- year_2023/lib/day_08.ex
+++ year_2023/lib/day_08.ex
@@ -30,7 +30,34 @@ defmodule Day08 do
     |> Kernel.+(1)
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    [turns, map] = input.contents_of(8) |> String.split("\n\n")
 
+    map = map
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(&Parser.map_line/1)
+    |> Enum.map(fn {:ok, [node, left, right], "", %{}, _, _} -> {node, %{"L" => left, "R" => right}} end)
+    |> Map.new
+
+    map
+    |> Map.keys
+    |> Enum.filter(fn key -> String.last(key) == "A" end)
+    |> Enum.map(fn node ->
+      turns
+      |> String.split("", trim: true)
+      |> Stream.cycle
+      |> Stream.transform(Map.fetch!(map, node), fn
+        "L", %{"L" => <<_prefix::binary-size(2)>><>"Z"} -> {:halt, nil}
+        "R", %{"R" => <<_prefix::binary-size(2)>><>"Z"} -> {:halt, nil}
+        "L", %{"L" => next_node} -> {[1], Map.fetch!(map, next_node)}
+        "R", %{"R" => next_node} -> {[1], Map.fetch!(map, next_node)}
+      end)
+      |> Enum.sum
+      |> Kernel.+(1)
+    end)
+    |> Enum.map(fn x -> div(x, String.length(turns)) end)
+    |> Enum.reduce(fn x, y -> x * y end)
+    |> Kernel.*(String.length(turns))
   end
 end
--- year_2023/test/day_08_test.exs
+++ year_2023/test/day_08_test.exs
@@ -4,9 +4,4 @@ defmodule Day08Test do
   test "it can solve part one" do
     assert Day08.part_one(InputTestFile) == 2
   end
-
-  @tag :skip
-  test "it can solve part two" do
-    assert Day08.part_two(InputTestFile) == nil
-  end
 end
