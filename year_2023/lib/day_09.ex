3a3e54e0b3e27c195913aad0e6549e059a1c8d36
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2023-12-09 07:15:15 -0600

    Day 09, Part 1

---
+++ year_2023/lib/day_09.ex
@@ -0,0 +1,31 @@
+defmodule Day09 do
+  def part_one(input \\ InputFile) do
+    input.contents_of(9, :stream)
+    |> Enum.map(&next_in/1)
+    |> Enum.sum
+  end
+
+  def part_two(_input \\ InputFile) do
+
+  end
+
+  def next_in(raw_series) do
+    raw_series
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> reduce([])
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(&Kernel.hd/1)
+    |> Enum.sum()
+  end
+
+  def reduce(series, reductions) do
+    if Enum.all?(series, &(&1 == 0)), do: reductions, else: series |> deltas() |> reduce([series | reductions])
+  end
+
+  def deltas(series) do
+    series
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> b - a end)
+  end
+end
---
+++ year_2023/test/day_09_test.exs
@@ -0,0 +1,22 @@
+defmodule Day09Test do
+  use ExUnit.Case, async: true
+
+  test "it can solve part one" do
+    assert Day09.part_one(InputTestFile) == 114
+  end
+
+  @tag :skip
+  test "it can solve part two" do
+    assert Day09.part_two(InputTestFile) == nil
+  end
+
+  test "it can find the next value for a series" do
+    assert Day09.next_in("0 3 6 9 12 15") == 18
+    assert Day09.next_in("1 3 6 10 15 21") == 28
+    assert Day09.next_in("10 13 16 21 30 45") == 68
+  end
+
+  test "it can compute deltas between numbers" do
+    assert Day09.deltas([0, 3, 6, 9, 12, 15]) == [3, 3, 3, 3, 3]
+  end
+end
