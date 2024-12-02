b1d70d9dfea64a29478d8f33e5078943e6ebea29
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-01 22:21:52 -0600

    Day 1

--- year_2024/lib/day_01.ex
+++ year_2024/lib/day_01.ex
@@ -1,9 +1,28 @@
 defmodule Day01 do
   def part_one(input \\ InputFile) do
+    {l, r} = input.contents_of(1, :stream)
+    |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.unzip()
 
+    l = Enum.sort(l)
+    r = Enum.sort(r)
+
+    Enum.zip(l, r)
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.reduce(&Kernel.+/2)
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    {l, r} = input.contents_of(1, :stream)
+    |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.unzip()
 
+    counts = Enum.reduce(r, %{}, fn n, acc -> Map.update(acc, n, 1, fn v -> v + 1 end) end)
+
+    l
+    |> Enum.map(fn n -> n * Map.get(counts, n, 0) end)
+    |> Enum.reduce(&Kernel.+/2)
   end
 end
--- year_2024/test/day_01_test.exs
+++ year_2024/test/day_01_test.exs
@@ -2,11 +2,10 @@ defmodule Day01Test do
   use ExUnit.Case, async: true
 
   test "it can solve part one" do
-    assert Day01.part_one(InputTestFile) == nil
+    assert Day01.part_one(InputTestFile) == 11
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day01.part_two(InputTestFile) == nil
+    assert Day01.part_two(InputTestFile) == 31
   end
 end
