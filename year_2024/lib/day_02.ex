e219f26c0bf0124406144dc4e0a6b173b46f277d
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-02 09:59:00 -0600

    Ugly Day 2, Part 2

--- year_2024/lib/day_02.ex
+++ year_2024/lib/day_02.ex
@@ -5,8 +5,15 @@ defmodule Day02 do
     |> Enum.count(&valid?/1)
   end
 
-  def part_two(_input \\ InputFile) do
-
+  def part_two(input \\ InputFile) do
+    input.contents_of(2, :stream)
+    |> Enum.map(fn line -> line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.count(&valid_with_dampening?/1)
+    #|> Enum.reject(&valid_with_dampening?/1)
+    #|> Enum.find(&slow_detect/1)
+    #|> Enum.join(",")
+    #|> IO.inspect()
+    #|> Enum.each(fn arr -> IO.inspect(Enum.join(arr, ",")) end)
   end
 
   def valid?(arr) do
@@ -14,4 +21,32 @@ defmodule Day02 do
 
     Enum.all?(deltas, fn x -> x > 0 && x < 4 end) || Enum.all?(deltas, fn x -> x < 0 && x > -4 end)
   end
+
+  def slow_detect(arr), do: slow_detect([], hd(arr), tl(arr))
+  def slow_detect(pre, _elt, []), do: valid?(pre)
+  def slow_detect(pre, elt, rest) do
+    IO.inspect(elt)
+    if valid?(pre ++ rest), do: true, else: slow_detect(pre ++ [elt], hd(rest), tl(rest))
+  end
+
+  def valid_with_dampening?([a, b | rest]), do: valid_with_dampening?(nil, a, b, nil, rest, false)
+  def valid_with_dampening?(_prev, a, b, nil, rest, false) when a < b, do: valid_with_dampening?(nil, a, b, :inc, rest, false) || valid_with_dampening?(nil, b, hd(rest), nil, tl(rest), true) || valid_with_dampening?(nil, a, hd(rest), nil, tl(rest), true)
+  def valid_with_dampening?(_prev, a, b, nil, rest, false) when a > b, do: valid_with_dampening?(nil, a, b, :dec, rest, false) || valid_with_dampening?(nil, b, hd(rest), nil, tl(rest), true) || valid_with_dampening?(nil, a, hd(rest), nil, tl(rest), true)
+  def valid_with_dampening?(_prev, a, b, nil, rest, true) when a < b, do: valid_with_dampening?(nil, a, b, :inc, rest, true)
+  def valid_with_dampening?(_prev, a, b, nil, rest, true) when a > b, do: valid_with_dampening?(nil, a, b, :dec, rest, true)
+  def valid_with_dampening?(_prev, a, b, nil, rest, false) when a == b, do: valid_with_dampening?(nil, b, hd(rest), nil, tl(rest), true)
+  def valid_with_dampening?(_prev, a, b, nil, _rest, true) when a == b, do: false
+
+  def valid_with_dampening?(_prev, _a, _b, :inc, [], false), do: true
+  def valid_with_dampening?(_prev, a, b, :inc, [], _dampened) when b - a > 0 and b - a < 4, do: true
+  def valid_with_dampening?(_prev, a, b, :inc, rest, dampened) when b - a > 0 and b - a < 4, do: valid_with_dampening?(a, b, hd(rest), :inc, tl(rest), dampened)
+  def valid_with_dampening?(prev, a, b, :inc, rest, false), do: valid_with_dampening?(prev, a, hd(rest), :inc, tl(rest), true) || valid_with_dampening?(nil, prev, b, :inc, rest, true)
+  def valid_with_dampening?(_prev, _a, _b, :inc, _rest, true), do: false
+
+  def valid_with_dampening?(_prev, _a, _b, :dec, [], false), do: true
+  def valid_with_dampening?(_prev, a, b, :dec, [], _dampened) when b - a < 0 and b - a > -4, do: true
+  def valid_with_dampening?(_prev, a, b, :dec, rest, dampened) when b - a < 0 and b - a > -4, do: valid_with_dampening?(a, b, hd(rest), :dec, tl(rest), dampened)
+  def valid_with_dampening?(prev, a, b, :dec, rest, false), do: valid_with_dampening?(prev, a, hd(rest), :dec, tl(rest), true) || valid_with_dampening?(nil, prev, b, :dec, rest, true)
+  def valid_with_dampening?(_prev, _a, _b, :dec, _rest, true), do: false
+
 end
--- year_2024/test/day_02_test.exs
+++ year_2024/test/day_02_test.exs
@@ -5,9 +5,8 @@ defmodule Day02Test do
     assert Day02.part_one(InputTestFile) == 2
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day02.part_two(InputTestFile) == nil
+    assert Day02.part_two(InputTestFile) == 4
   end
 
   test "an increasing array is valid" do
@@ -32,5 +31,32 @@ defmodule Day02Test do
 
   test "an array that increases more than 3 at a time is not valid" do
     refute Day02.valid?([1, 2, 6])
+  end
+
+  test "removing one value can make an array valid" do
+    assert Day02.valid_with_dampening?([1, 3, 2, 4, 5])
+    assert Day02.valid_with_dampening?([8, 6, 4, 4, 1])
+    assert Day02.valid_with_dampening?([3, 3, 4, 5, 7])
+  end
+
+  test "multiple identical values cannot be dampened" do
+    refute Day02.valid_with_dampening?([3, 3, 3, 4, 5])
+  end
+
+  test "dampening the final element works" do
+    assert Day02.valid_with_dampening?([38, 41, 44, 47, 50, 48])
+  end
+
+  test "it can swith from increasing to decreasing with dampening" do
+    assert Day02.valid_with_dampening?([2, 3, 2, 1])
+  end
+
+  test "it can switch from descreasing to increasing with dampening" do
+    assert Day02.valid_with_dampening?([2, 1, 2, 3])
+    assert Day02.valid_with_dampening?([4, 1, 5, 6, 9])
+  end
+
+  test "it checks" do
+    assert Day02.valid_with_dampening?([1, 2, 5, 4, 5])
   end
 end
