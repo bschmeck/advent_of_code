00198445d03aade36fc3f64324d6129a0b056c90
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-05 09:22:15 -0600

    Day 5, Part 2

--- year_2024/lib/day_05.ex
+++ year_2024/lib/day_05.ex
@@ -16,7 +16,22 @@ defmodule Day05 do
     |> Enum.reduce(&Kernel.+/2)
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    [raw_rules, raw_pages] = input.contents_of(5) |> String.split("\n\n")
+
+    rules = raw_rules
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, "|"))
+    |> Enum.reduce(%{}, fn [pre, post], acc -> Map.update(acc, pre, MapSet.new([post]), &MapSet.put(&1, post)) end)
+
+    raw_pages
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, ",", trim: true))
+    |> Enum.reject(fn pages -> valid_order?(pages, rules, MapSet.new()) end)
+    |> Enum.map(&sort(&1, rules))
+    |> Enum.map(&midpoint/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(&Kernel.+/2)
 
   end
 
@@ -24,6 +39,26 @@ defmodule Day05 do
   def valid_order?([page | rest], rules, seen) do
     postreqs = Map.get(rules, page, MapSet.new())
     if MapSet.intersection(postreqs, seen) |> Enum.any?, do: false, else: valid_order?(rest, rules, MapSet.put(seen, page))
+  end
+
+  def sort(pages, rules) do
+    # Find page with no pre-reqs, add to sorted and recurse
+    page_set = MapSet.new(pages)
+
+    pages
+    |> Enum.reduce(%{}, fn page, acc ->
+      postreqs = Map.get(rules, page, MapSet.new())
+      prereqs = MapSet.difference(page_set, postreqs) |> MapSet.delete(page) |> MapSet.to_list() |> Enum.sort()
+      Map.put(acc, prereqs, page)
+    end)
+    |> build([])
+  end
+
+  def build(prereqs, sorted) when prereqs == %{}, do: sorted
+  def build(prereqs, sorted) do
+    key = Enum.sort(sorted)
+    page = Map.get(prereqs, key)
+    build(Map.delete(prereqs, key), [page | sorted])
   end
 
   def midpoint(arr) do
--- year_2024/test/day_05_test.exs
+++ year_2024/test/day_05_test.exs
@@ -5,8 +5,7 @@ defmodule Day05Test do
     assert Day05.part_one(InputTestFile) == 143
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day05.part_two(InputTestFile) == nil
+    assert Day05.part_two(InputTestFile) == 123
   end
 end
