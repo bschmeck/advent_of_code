dc4ec661b8badf63c141330d4783d8931e665c97
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-08 15:35:09 -0600

    Day 7, Part 2

--- year_2024/lib/day_07.ex
+++ year_2024/lib/day_07.ex
@@ -7,19 +7,36 @@ defmodule Day07 do
     |> Enum.reduce(&Kernel.+/2)
   end
 
-  def part_two(_input \\ InputFile) do
-
+  def part_two(input \\ InputFile) do
+    input.contents_of(7, :stream)
+    |> Enum.map(&parse/1)
+    |> Enum.filter(&valid2?/1)
+    |> Enum.map(&(elem(&1, 0)))
+    |> Enum.reduce(&Kernel.+/2)
   end
 
   def parse(line) do
     [value, raw] = String.split(line, ": ")
     value = String.to_integer(value)
-    [first | rest] = String.split(raw, " ", trim: true) |> Enum.map(&String.to_integer/1)
+    nums = String.split(raw, " ", trim: true) |> Enum.map(&String.to_integer/1)
 
-    {value, first, rest}
+    {value, nums}
   end
 
-  def valid?({value, total, []}), do: value == total
-  def valid?({value, total, _rest}) when value < total, do: false
-  def valid?({value, total, [num | rest]}), do: valid?({value, total + num, rest}) || valid?({value, total * num, rest})
+  def valid?({value, [num]}), do: value == num
+  def valid?({value, [a, b | rest]}) do
+    valid?({value, [a + b | rest]}) ||
+      valid?({value, [a * b | rest]})
+  end
+
+  def valid2?({value, [num]}), do: value == num
+  def valid2?({value, [a, b | rest]}) do
+    valid2?({value, [a + b | rest]}) ||
+      valid2?({value, [a * b | rest]}) ||
+      valid2?({value, [combine(a, b) | rest]})
+  end
+
+  def combine(a, b) when b < 10, do: a * 10 + b
+  def combine(a, b) when b < 100, do: a * 100 + b
+  def combine(a, b) when b < 1000, do: a * 1000 + b
 end
