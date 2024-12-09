e7d4cc62fcced760ddfd3b138db1748522740450
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-08 21:00:58 -0600

    Day 8, Part 2

--- year_2024/lib/day_08.ex
+++ year_2024/lib/day_08.ex
@@ -20,8 +20,25 @@ defmodule Day08 do
     |> Enum.count()
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    lines = input.contents_of(8) |> String.split("\n", trim: true)
 
+    width = hd(lines) |> String.split("", trim: true) |> Enum.count()
+    height = Enum.count(lines)
+
+    lines
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {row, y}, map ->
+      String.split(row, "", trim: true)
+      |> Enum.with_index()
+      |> Enum.reduce(map, fn
+        {".", _x}, m -> m
+        {freq, x}, m -> Map.update(m, freq, [{x, y}], fn pts -> [{x, y} | pts] end)
+      end)
+    end)
+    |> Enum.flat_map(&(antinodes2(&1, width, height)))
+    |> Enum.into(MapSet.new())
+    |> Enum.count()
   end
 
   def antinodes({_freq, [_point]}, _w, _h), do: []
@@ -30,6 +47,20 @@ defmodule Day08 do
     |> pairs([])
     |> Enum.flat_map(fn {{x1, y1}, {x2, y2}} -> [{2 * x1 - x2, 2 * y1 - y2}, {2 * x2 - x1, 2 * y2 - y1}] end)
     |> Enum.reject(fn {x, y} -> x < 0 || x >= w || y < 0 || y >= h end)
+  end
+
+  def antinodes2({_freq, [p]}, _w, _h), do: [p]
+  def antinodes2({_freq, points}, w, h) do
+    points
+    |> pairs([])
+    |> Enum.flat_map(fn {p1, p2} -> points(p1, p2, w, h) ++ points(p2, p1, w, h) end)
+  end
+
+  def points({x1, y1}, {x2, y2}, w, h) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(fn i -> {(i + 1) * x1 - i * x2, (i + 1) * y1 - i * y2} end)
+    |> Stream.take_while(fn {x, y} -> (x >= 0 && x < w) && (y >= 0 && y < h) end)
+    |> Enum.to_list()
   end
 
   def pairs([_pt], acc), do: acc
--- year_2024/test/day_08_test.exs
+++ year_2024/test/day_08_test.exs
@@ -5,12 +5,15 @@ defmodule Day08Test do
     assert Day08.part_one(InputTestFile) == 14
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day08.part_two(InputTestFile) == nil
+    assert Day08.part_two(InputTestFile) == 34
   end
 
   test "it can compute pairs" do
     assert Day08.pairs([1, 2, 3, 4], []) |> Enum.sort() == [{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}]
+  end
+
+  test "it can find points in the grid" do
+    assert Day08.points({2, 2}, {3, 3}, 5, 5) |> Enum.sort == [{0, 0}, {1, 1}, {2, 2}]
   end
 end
