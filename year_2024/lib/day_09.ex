2762e4bc87274e699b5228221e9d6174a47926b7
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-09 19:02:34 -0600

    Account for free spaces when computing a checksum

--- year_2024/lib/day_09.ex
+++ year_2024/lib/day_09.ex
@@ -11,32 +11,31 @@ defmodule Day09 do
   end
 
   def part_two(_input \\ InputFile) do
-
   end
 
   def compact([], compacted), do: Enum.reverse(compacted)
-  def compact([{[len, _free], n}], compacted), do: Enum.reverse([[n, len] | compacted])
+  def compact([{[len, _free], n}], compacted), do: Enum.reverse([[n, len, 0] | compacted])
   def compact([{[len, _free], n} | _rest]=drive, compacted) do
     [last | rest] = Enum.reverse(drive)
-    compact(Enum.reverse(rest), last, [[n, len] | compacted])
+    compact(Enum.reverse(rest), last, [[n, len, 0] | compacted])
   end
 
   def compact([{[_len, free], _n} | rest], {[last_len, _last_free], last_n}, compacted) when free == last_len do
-      compact(rest, [[last_n, free] | compacted])
+      compact(rest, [[last_n, free, 0] | compacted])
   end
   def compact([{[_len, free], _n} | rest], {[last_len, last_free], last_n}, compacted) when free < last_len do
-    compact(rest ++ [{[last_len - free, last_free], last_n}], [[last_n, free] | compacted])
+    compact(rest ++ [{[last_len - free, last_free], last_n}], [[last_n, free, 0] | compacted])
   end
   def compact([{[_len, free], _n}], {[last_len, _last_free], last_n}, compacted) when free > last_len do
-    compact([], [[last_n, last_len] | compacted])
+    compact([], [[last_n, last_len, 0] | compacted])
   end
   def compact([{[len, free], n} | rest], {[last_len, _last_free], last_n}, compacted) when free > last_len do
     [next_last | rest_rev] = Enum.reverse(rest)
     rest = [{[len, free - last_len], n} | Enum.reverse(rest_rev)]
-    compact(rest, next_last, [[last_n, last_len] | compacted])
+    compact(rest, next_last, [[last_n, last_len, 0] | compacted])
   end
 
   def checksum(drive), do: checksum(drive, 0, 0)
   def checksum([], _pos, total), do: total
-  def checksum([[n, len] | rest], pos, total), do: checksum(rest, pos + len, div(n * len * (2 * pos + len - 1), 2) + total)
+  def checksum([[n, len, free] | rest], pos, total), do: checksum(rest, pos + len + free, div(n * len * (2 * pos + len - 1), 2) + total)
 end
--- year_2024/test/day_09_test.exs
+++ year_2024/test/day_09_test.exs
@@ -5,13 +5,12 @@ defmodule Day09Test do
     assert Day09.part_one(InputTestFile) == 1928
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day09.part_two(InputTestFile) == nil
+    assert Day09.part_two(InputTestFile) == 2858
   end
 
   test "it can compute checksums" do
-    drive = [[0, 2], [9, 2], [8, 1], [1, 3], [8, 3], [2, 1], [7, 3], [3, 3], [6, 1], [4, 2], [6, 1], [5, 4], [6, 2]]
+    drive = [[0, 2, 0], [9, 2, 0], [8, 1, 0], [1, 3, 0], [8, 3, 0], [2, 1, 0], [7, 3, 0], [3, 3, 0], [6, 1, 0], [4, 2, 0], [6, 1, 0], [5, 4, 0], [6, 2, 0]]
     assert Day09.checksum(drive) == 1928
   end
 end
