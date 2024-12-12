883be12459fe175064e438326dda8e2fd093e2ec
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2024-12-12 09:20:45 -0600

    Day 9, Part 2

--- year_2024/lib/day_09.ex
+++ year_2024/lib/day_09.ex
@@ -1,4 +1,12 @@
 defmodule Day09 do
+  defmodule File do
+    defstruct [:number, :length]
+  end
+
+  defmodule Free do
+    defstruct [:length]
+  end
+
   def part_one(input \\ InputFile) do
     input.contents_of(9)
     |> String.trim()
@@ -10,7 +18,19 @@ defmodule Day09 do
     |> checksum()
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    input.contents_of(9)
+    |> String.trim()
+    |> String.split("", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(2, 2, [0])
+    |> Enum.with_index()
+    |> Enum.flat_map(fn
+      {[len, 0], n} -> [%File{number: n, length: len}]
+      {[len, free], n} -> [%File{number: n, length: len}, %Free{length: free}]
+    end)
+    |> compact2([])
+    |> checksum2(0, 0)
   end
 
   def compact([], compacted), do: Enum.reverse(compacted)
@@ -35,7 +55,27 @@ defmodule Day09 do
     compact(rest, next_last, [[last_n, last_len, 0] | compacted])
   end
 
+  def compact2([], tail), do: tail
+  def compact2(drive, tail) do
+    [last | rest] = Enum.reverse(drive)
+
+    {drive, last} = place(Enum.reverse(rest), last)
+    compact2(drive, [last | tail])
+  end
+
+  def place(drive, %Free{}=free), do: {drive, free}
+  def place(drive, file), do: place(drive, file, [])
+  def place([], file, head), do: {Enum.reverse(head), file}
+  def place([%File{}=existing | rest], file, head), do: place(rest, file, [existing | head])
+  def place([%Free{length: space}=free | rest], %File{length: len}=file, head) when len > space, do: place(rest, file, [free | head])
+  def place([%Free{length: space} | rest], %File{length: len}=file, head) when len == space, do: {Enum.reverse(head) ++ [file | rest], %Free{length: len}}
+  def place([%Free{length: space} | rest], %File{length: len}=file, head) when len < space, do: {Enum.reverse(head) ++ [file, %Free{length: space - len} | rest], %Free{length: len}}
+
   def checksum(drive), do: checksum(drive, 0, 0)
   def checksum([], _pos, total), do: total
   def checksum([[n, len, free] | rest], pos, total), do: checksum(rest, pos + len + free, div(n * len * (2 * pos + len - 1), 2) + total)
+
+  def checksum2([], _pos, total), do: total
+  def checksum2([%Free{length: len} | rest], pos, total), do: checksum2(rest, pos + len, total)
+  def checksum2([%File{number: n, length: len} | rest], pos, total), do: checksum2(rest, pos + len, div(n * len * (2 * pos + len - 1), 2) + total)
 end
