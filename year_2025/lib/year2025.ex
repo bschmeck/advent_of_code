cc8b8bfbb1ddf9dd871c438fc222aa9b72237eee
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2025-12-01 07:52:52 -0600

    Start Year 2025

---
+++ year_2025/lib/year2025.ex
@@ -0,0 +1,18 @@
+defmodule Year2025 do
+  @moduledoc """
+  Documentation for `Year2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Year2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ year_2025/test/year2025_test.exs
@@ -0,0 +1,8 @@
+defmodule Year2025Test do
+  use ExUnit.Case
+  doctest Year2025
+
+  test "greets the world" do
+    assert Year2025.hello() == :world
+  end
+end
