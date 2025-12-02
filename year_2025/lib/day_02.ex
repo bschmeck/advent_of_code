d4504a63615fd0f23bf40f90f0a325a974727979
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2025-12-02 16:07:37 -0600

    Day 2.2
    Slow string manipulations

--- year_2025/lib/day_02.ex
+++ year_2025/lib/day_02.ex
@@ -8,8 +8,13 @@ defmodule Day02 do
     |> Enum.reduce(0, &Kernel.+/2)
   end
 
-  def part_two(_input \\ InputFile) do
-
+  def part_two(input \\ InputFile) do
+    input.contents_of(2)
+    |> String.trim_trailing()
+    |> String.split(",")
+    |> Enum.map(&String.split(&1, "-"))
+    |> Enum.flat_map(&repeats/1)
+    |> Enum.reduce(0, &Kernel.+/2)
   end
 
   def mirrors([from, to]), do: mirrors(String.to_integer(from), String.to_integer(to))
@@ -27,4 +32,37 @@ defmodule Day02 do
       _ -> false
     end
   end
+
+  def repeats([from, to]), do: repeats(String.to_integer(from), String.to_integer(to))
+  def repeats(from, to), do: repeats(from, to, [])
+  def repeats(from, to, acc) when from > to, do: acc
+  def repeats(from, to, acc) do
+    if repeat?(from), do: repeats(from + 1, to, [from | acc]), else: repeats(from + 1, to, acc)
+  end
+
+  def repeat?(n) when n < 11, do: false
+  def repeat?(n) do
+    digits = n |> Integer.to_string() |> String.split("", trim: true)
+    repeat?(digits, factors(Enum.count(digits)))
+  end
+  def repeat?(_digits, []), do: false
+  def repeat?(digits, [n | rest]) do
+    case digits |> Enum.chunk_every(n) |> Enum.uniq() |> Enum.count() do
+      1 -> true
+      _ -> repeat?(digits, rest)
+    end
+  end
+
+  def factors(1), do: [1]
+  def factors(2), do: [1]
+  def factors(3), do: [1]
+  def factors(4), do: [1, 2]
+  def factors(5), do: [1]
+  def factors(6), do: [1, 2, 3]
+  def factors(7), do: [1]
+  def factors(8), do: [1, 2, 4]
+  def factors(9), do: [1, 3]
+  def factors(10), do: [1, 2, 5]
+  def factors(11), do: [1]
+  def factors(n), do: Enum.filter(1..div(n, 2), &(rem(n, &1) == 0))
 end
--- year_2025/test/day_02_test.exs
+++ year_2025/test/day_02_test.exs
@@ -23,8 +23,35 @@ defmodule Day02Test do
     assert Day02.mirror?(123123)
   end
 
-  @tag :skip
+  test "it can find repeat numbers in a range" do
+    assert Day02.repeats(11, 22) |> Enum.sort() == [11, 22]
+    assert Day02.repeats(95, 115) |> Enum.sort() == [99, 111]
+    assert Day02.repeats(998, 1012) |> Enum.sort() == [999, 1010]
+    assert Day02.repeats(1188511880, 1188511890) == [1188511885]
+    assert Day02.repeats(222220, 222224) == [222222]
+    assert Day02.repeats(1698522, 1698528) == []
+    assert Day02.repeats(446443, 446449) == [446446]
+    assert Day02.repeats(38593856, 38593862) == [38593859]
+    assert Day02.repeats(565653, 565659) == [565656]
+    assert Day02.repeats(824824821, 824824827) == [824824824]
+    assert Day02.repeats(2121212118, 2121212124) == [2121212121]
+  end
+
+  test "it can identify repeat numbers" do
+    assert Day02.repeat?(11)
+    refute Day02.repeat?(12)
+    refute Day02.repeat?(121)
+    assert Day02.repeat?(123123)
+    refute Day02.repeat?(4)
+  end
+
+  test "it can find factors" do
+    assert Day02.factors(8) == [1, 2, 4]
+    assert Day02.factors(9) == [1, 3]
+    assert Day02.factors(10) == [1, 2, 5]
+  end
+
   test "it can solve part two" do
-    assert Day02.part_two(InputTestFile) == nil
+    assert Day02.part_two(InputTestFile) == 4174379265
   end
 end
