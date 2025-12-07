bc44a90d6ef668929c183617f67584b2d8aa654b
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2025-12-06 21:29:40 -0600

    Day 5, part 2

--- year_2025/lib/day_05.ex
+++ year_2025/lib/day_05.ex
@@ -12,9 +12,28 @@ defmodule Day05 do
     |> Enum.count(&(fresh?(ranges, &1)))
   end
 
-  def part_two(_input \\ InputFile) do
+  def part_two(input \\ InputFile) do
+    [fresh, _avail] = input.contents_of(5) |> String.split("\n\n")
 
+    fresh
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn r -> String.split(r, "-") |> Enum.map(&String.to_integer/1) end)
+    |> combine()
+    |> Enum.map(&size/1)
+    |> Enum.reduce(&Kernel.+/2)
   end
+
+  def combine([range | rest]), do: combine(rest, [range], [])
+  def combine([], acc, []), do: acc
+  def combine([range | rest], [], tmp), do: combine(rest, [range | tmp], [])
+  def combine([[_min_a, max_a] | _rest] = ranges, [[min_b, max_b] | acc], tmp) when max_a < min_b, do: combine(ranges, acc, [[min_b, max_b] | tmp])
+  def combine([[min_a, _max_a] | _rest] = ranges, [[min_b, max_b] | acc], tmp) when min_a > max_b, do: combine(ranges, acc, [[min_b, max_b] | tmp])
+  def combine([[min_a, max_a] | rest], [[min_b, max_b] | acc], tmp) when min_a >= min_b and max_a <= max_b, do: combine(rest, [[min_b, max_b] | acc] ++ tmp, [])
+  def combine([[min_a, max_a] | _rest] = ranges, [[min_b, max_b] | acc], tmp) when min_a <= min_b and max_a >= max_b, do: combine(ranges, acc ++ tmp, [])
+  def combine([[min_a, max_a] | rest], [[min_b, max_b] | acc], tmp) when min_a < min_b and max_a <= max_b, do: combine([[min_a, max_b] | rest], tmp ++ acc, [])
+  def combine([[min_a, max_a] | rest], [[min_b, max_b] | acc], tmp) when min_a >= min_b and max_a > max_b, do: combine([[min_b, max_a] | rest], tmp ++ acc, [])
+
+  def size([min, max]), do: max - min + 1
 
   def fresh?([], _ingredient), do: false
   def fresh?([[min, max] | _rest], ingredient) when min <= ingredient and max >= ingredient, do: true
--- year_2025/test/day_05_test.exs
+++ year_2025/test/day_05_test.exs
@@ -5,8 +5,18 @@ defmodule Day05Test do
     assert Day05.part_one(InputTestFile) == 3
   end
 
-  @tag :skip
   test "it can solve part two" do
-    assert Day05.part_two(InputTestFile) == nil
+    assert Day05.part_two(InputTestFile) == 14
+  end
+
+  test "it can combine ranges" do
+    assert Day05.combine([[1, 5], [10, 15]]) |> Enum.sort == Enum.sort([[1, 5], [10, 15]])
+    assert Day05.combine([[10, 15], [1, 5]]) |> Enum.sort == Enum.sort([[1, 5], [10, 15]])
+    assert Day05.combine([[1, 5], [2, 4]]) |> Enum.sort == Enum.sort([[1, 5]])
+    assert Day05.combine([[2, 4], [1, 5]]) |> Enum.sort == Enum.sort([[1, 5]])
+    assert Day05.combine([[2, 5], [1, 3]]) |> Enum.sort == Enum.sort([[1, 5]])
+    assert Day05.combine([[1, 3], [2, 5]]) |> Enum.sort == Enum.sort([[1, 5]])
+    assert Day05.combine([[1, 3], [2, 5], [10, 15]]) |> Enum.sort == Enum.sort([[1, 5], [10, 15]])
+    assert Day05.combine([[1, 5], [7, 10], [3, 8]]) |> Enum.sort == Enum.sort([[1, 10]])
   end
 end
